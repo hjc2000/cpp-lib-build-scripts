701f5172ed5cba6a052edb8d1c0fa32dffb09566
hjc2000 <huangjincanqqmail@qq.com>
2024-02-19 00:21:15 +0800

    1

--- ffmpeg/msys2-ucrt-build-sdl2.sh
+++ ffmpeg/msys2-ucrt-build-sdl2.sh
@@ -1,5 +1,5 @@
-source_root_path=$(pwd)
-install_path=${source_root_path}/msys2-ucrt-build-install/
+export source_root_path=$(pwd)
+export install_path=${source_root_path}/msys2-ucrt-build-install/
 
 get-repo.sh https://gitee.com/mycn027b/SDL.git SDL2 &&
 cd SDL &&
@@ -7,10 +7,20 @@ if [ ! -d "./build/" ]; then
 	mkdir build
 fi &&
 cd build &&
-rm -rf * &&
-cmake -G "Visual Studio 17 2022" .. \
+rm -rf *
+
+cmd
+cmd.exe /k "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -startdir=none -arch=x64 -host_arch=x64
+"C:\msys64\usr\bin\bash"
+
+touch toolchain.cmake &&
+echo "set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_C_COMPILER cl.exe)
+set(CMAKE_CXX_COMPILER cl.exe)" > toolchain.cmake &&
+
+cmake -G "Unix Makefiles" .. \
+-DCMAKE_TOOLCHAIN_FILE=$(pwd)/toolchain.cmake \
 -DCMAKE_INSTALL_PREFIX=${install_path}
 
-# 如果使用 makefile 进行构建，SDL 会检测一些头文件，结果就绑死了 mysy2 了，
-# 编译出来的库在 vs 中无法使用。
-# 所以需要手动打开 vs 进行编译。本脚本只能做到这了。
+"/c/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/Ninja/ninja.exe" -h
