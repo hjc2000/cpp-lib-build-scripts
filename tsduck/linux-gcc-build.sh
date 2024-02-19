92e039db83ffd46524d924d9739ba2a3dd5e5585
hjc2000 <huangjincanqqmail@qq.com>
2024-02-19 20:40:25 +0800

    auto commit

--- tsduck/linux-gcc-build.sh
+++ tsduck/linux-gcc-build.sh
@@ -8,8 +8,6 @@ cd ${source_root_path}/tsduck &&
 # 这里面的很多依赖与系统的现有项冲突，所以就不要安装了。
 # 经过测试，按照下文中的命令进行构建，禁用一些模块，就可以不用执行此脚本安装依赖。
 
-# make clean &&
-
 #  Additional options which can be defined:
 #
 #  - NOTEST     : Do not build unitary tests.
@@ -24,6 +22,7 @@ cd ${source_root_path}/tsduck &&
 #  - NOGITHUB   : No version check, no download, no upgrade from GitHub.
 #  - NOHWACCEL  : Disable hardware acceleration such as crypto instructions.
 #  - NOPCSTD    : Remove the std=c++17 flag from libtsduck's pkg-config file.
+make clean &&
 make -j12 NOGITHUB=1 NOVATEK=1 NOTEST=1 NODEKTEC=1 NOHIDES=1 NOCURL=1 NOEDITLINE=1 NOSRT=1 NORIST=1 &&
 make install SYSPREFIX=${install_path} &&
 
