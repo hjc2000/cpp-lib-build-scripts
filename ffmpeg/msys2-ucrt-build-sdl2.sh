7ea69a7c814f7981e2eabaa6a5779e8303940291
hjc2000 <huangjincanqqmail@qq.com>
2024-02-19 11:25:48 +0800

    1

--- ffmpeg/msys2-ucrt-build-sdl2.sh
+++ ffmpeg/msys2-ucrt-build-sdl2.sh
@@ -1,25 +1,22 @@
-export source_root_path=$(pwd)
-export install_path=${source_root_path}/msys2-ucrt-build-install/
+source_root_path=$(pwd)
+install_path=${source_root_path}/msys2-ucrt-build-install/
 
 get-repo.sh https://gitee.com/mycn027b/SDL.git SDL2 &&
 cd SDL &&
-if [ ! -d "./build/" ]; then
+
+if [ ! -d ./build/ ]; then
 	mkdir build
 fi &&
-cd build &&
-rm -rf *
-export build_path=$(pwd)
+cd build && rm -rf *
+
 touch toolchain.cmake &&
 echo "set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-set(CMAKE_C_COMPILER cl.exe)
-set(CMAKE_CXX_COMPILER cl.exe)" > toolchain.cmake &&
+set(CMAKE_SYSTEM_PROCESSOR x64)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)" > toolchain.cmake &&
 
-cmd
-cmd.exe /k "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -startdir=none -arch=x64 -host_arch=x64
-cmake -G "Ninja" .. ^
--DCMAKE_TOOLCHAIN_FILE=%build_path%/toolchain.cmake ^
--DCMAKE_INSTALL_PREFIX=%install_path%
+cmake -G "Unix Makefiles" .. \
+-DCMAKE_TOOLCHAIN_FILE=$(pwd)/toolchain.cmake \
+-DCMAKE_INSTALL_PREFIX=${install_path} &&
 
-"C:\msys64\usr\bin\bash"
-# "/c/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/Ninja/ninja.exe" -h
+make -j12 && make install
