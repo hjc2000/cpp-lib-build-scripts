92a5d10a0ebb6d7fa84ad73c822b2d85645aad86
hjc2000 <huangjincanqqmail@qq.com>
2024-02-19 13:56:01 +0800

    msys2-build-x265.sh

---
+++ ffmpeg/msys2-build-x265.sh
@@ -0,0 +1,20 @@
+source_root_path=$(pwd)
+install_path="${source_root_path}/msys2-build-install/"
+
+get-repo.sh https://gitee.com/Qianshunan/x265_git.git &&
+cd x265_git/source &&
+
+if [ ! -d ./build/ ]; then
+	mkdir build
+fi &&
+
+cd build && rm -rf *
+
+cmake -G "Unix Makefiles" .. \
+-DCMAKE_INSTALL_PREFIX="${install_path}" \
+-DENABLE_SHARED=on \
+-DENABLE_PIC=on \
+-DENABLE_ASSEMBLY=off &&
+
+make -j12 &&
+make install
