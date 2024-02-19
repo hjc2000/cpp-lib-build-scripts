8931301b59c82a1553d59c734f8a8e1ead3824aa
hjc2000 <huangjincanqqmail@qq.com>
2024-02-19 16:41:29 +0800

    auto commit

--- tsduck/msys2-build.sh
+++ tsduck/msys2-build.sh
@@ -1,4 +1,5 @@
 source_root_path=$(pwd)
 install_path="${source_root_path}/install/msys2-build-install/"
 
-cp -r ${source_root_path}/windows-prebuild-tsduck/ ${install_path}
+mkdir -p ${install_path}
+cp -r ${source_root_path}/windows-prebuild-tsduck/* ${install_path}
