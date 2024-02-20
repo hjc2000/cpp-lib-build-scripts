7f73008b64654705475318c809e752909ab10c71
hjc2000 <huangjincanqqmail@qq.com>
2024-02-20 17:00:10 +0800

    1

--- msys2/build-ffmpeg.sh
+++ msys2/build-ffmpeg.sh
@@ -5,18 +5,16 @@ export cpp_lib_build_scripts_path=$(cygpath ${cpp_lib_build_scripts_path})
 
 install_path="${libs_path}/ffmpeg"
 
-# 编译依赖
-${cpp_lib_build_scripts_path}/msys2/build-x264.sh &&
-${cpp_lib_build_scripts_path}/msys2/build-x265.sh &&
-${cpp_lib_build_scripts_path}/msys2/build-sdl2.sh &&
-${cpp_lib_build_scripts_path}/msys2/build-amf.sh
-
 cd ${repos_path} &&
 get-repo.sh https://gitee.com/programmingwindows/FFmpeg.git release/6.1 &&
 cd ${repos_path}/FFmpeg/ &&
 
 
-
+# 编译依赖
+${cpp_lib_build_scripts_path}/msys2/build-x264.sh &&
+${cpp_lib_build_scripts_path}/msys2/build-x265.sh &&
+${cpp_lib_build_scripts_path}/msys2/build-sdl2.sh &&
+${cpp_lib_build_scripts_path}/msys2/build-amf.sh
 
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${libs_path}/x264/lib/pkgconfig
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${libs_path}/x265/lib/pkgconfig
@@ -27,6 +25,8 @@ export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${libs_path}/SDL2/lib/pkgconfig
 --extra-cflags="-I${libs_path}/amf/include -DAMF_CORE_STATICTIC" \
 --enable-libx264 \
 --enable-libx265 \
+--disable-lzma \
+--disable-zlib \
 --enable-amf \
 --enable-pic \
 --enable-gpl \
