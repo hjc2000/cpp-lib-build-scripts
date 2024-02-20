acd99ceb3a3a913e17e101efcbf00fda16448214
hjc2000 <huangjincanqqmail@qq.com>
2024-02-20 15:41:33 +0800

    build-libiconv.sh

---
+++ msys2/build-libiconv.sh
@@ -0,0 +1,14 @@
+# 将依赖的环境变量转换成 msys 格式，然后导出。
+export libs_path=$(cygpath ${libs_path})
+export repos_path=$(cygpath ${repos_path})
+export cpp_lib_build_scripts_path=$(cygpath ${cpp_lib_build_scripts_path})
+
+install_path="${libs_path}/libiconv"
+
+cd ${repos_path} &&
+wget-repo.sh ${repos_path} https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz &&
+cd ${repos_path}/libiconv-1.17/ &&
+
+./configure --prefix=${install_path} &&
+
+make -j12 && make install
