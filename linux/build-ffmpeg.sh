b25ed589a78e522c8dea22b7b62f2fe99ef7c4c3
hjc2000 <huangjincanqqmail@qq.com>
2024-02-21 14:22:37 +0800

    1

--- linux/build-ffmpeg.sh
+++ linux/build-ffmpeg.sh
@@ -8,6 +8,16 @@ cd ${repos_path}
 get-repo.sh https://gitee.com/programmingwindows/FFmpeg.git release/6.1
 cd ${repos_path}/FFmpeg
 
+# 编译依赖
+${cpp_lib_build_scripts_path}/linux/build-x264.sh
+${cpp_lib_build_scripts_path}/linux/build-x265.sh
+${cpp_lib_build_scripts_path}/linux/build-sdl2.sh
+
+export PKG_CONFIG_PATH=""
+export PKG_CONFIG_PATH=${libs_path}/x264/lib/pkgconfig:${PKG_CONFIG_PATH}
+export PKG_CONFIG_PATH=${libs_path}/x265/lib/pkgconfig:${PKG_CONFIG_PATH}
+export PKG_CONFIG_PATH=${libs_path}/SDL2/lib/pkgconfig:${PKG_CONFIG_PATH}
+
 
 ./configure \
 --prefix="${install_path}" \
