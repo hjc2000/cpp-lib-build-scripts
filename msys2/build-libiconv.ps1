7ad43f1eea72f89d34135ea834b6646e3b94ac4d
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 11:32:13 +0800

    build-libiconv.ps1

--- msys2/build-libiconv.ps1
+++ msys2/build-libiconv.ps1
@@ -9,3 +9,16 @@ Set-Location $repos_path
 wget-repo.ps1 -workspace_dir $repos_path `
 	-repo_url https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz `
 	-out_dir_name libiconv
+
+$install_path = "$libs_path/libiconv"
+
+run-bash-cmd.ps1 -cmd @"
+set -e
+cd $(cygpath.exe $repos_path)/libiconv/libiconv-1.17/
+
+./configure \
+--prefix=$(cygpath.exe $install_path)
+
+make -j12
+make install
+"@
