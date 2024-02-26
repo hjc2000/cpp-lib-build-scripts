4c8f0ff1e51f79c4983f3d61588a28961d8bae1b
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 15:03:21 +0800

    1

---
+++ linux/build-ffmpeg.ps1
@@ -0,0 +1,44 @@
+param (
+	[string]$libs_path = $env:libs_path,
+	[string]$repos_path = $env:repos_path,
+	[string]$cpp_lib_build_scripts_path = $env:cpp_lib_build_scripts_path
+)
+$ErrorActionPreference = "Stop"
+
+# 编译依赖项
+& $cpp_lib_build_scripts_path/linux/build-x264.ps1
+& $cpp_lib_build_scripts_path/linux/build-x265.ps1
+& $cpp_lib_build_scripts_path/linux/build-sdl2.ps1
+
+$env:PKG_CONFIG_PATH = "$libs_path/x264/lib/pkgconfig:" +
+"$libs_path/x265/lib/pkgconfig:" +
+"$libs_path/SDL2/lib/pkgconfig:"
+Write-Host $env:PKG_CONFIG_PATH
+
+Push-Location $repos_path
+get-git-repo.ps1 -git_url "https://gitee.com/programmingwindows/FFmpeg.git" `
+	-branch_name release/6.1
+$source_path = "$repos_path/FFmpeg/"
+$install_path = "$libs_path/ffmpeg/"
+
+$cmd = @"
+set -e
+cd $source_path
+
+./configure \
+--prefix="$install_path" \
+--extra-cflags="-I$libs_path/amf/include/ -DAMF_CORE_STATICTIC" \
+--enable-libx264 \
+--enable-libx265 \
+--enable-pic \
+--enable-gpl \
+--enable-shared \
+--disable-static
+
+make clean
+make -j12
+make install
+"@
+
+$cmd | bash -norc
+Pop-Location
