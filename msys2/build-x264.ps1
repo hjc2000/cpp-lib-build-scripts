177f7e0c9f143b5761eabee970a13590f92e7ac4
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 14:20:55 +0800

    1

--- msys2/build-x264.ps1
+++ msys2/build-x264.ps1
@@ -6,7 +6,7 @@ param (
 $ErrorActionPreference = "Stop"
 
 Push-Location $repos_path
-get-git-repo.ps1 -git_url https://gitee.com/Qianshunan/x264.git
+get-git-repo.ps1 -git_url "https://gitee.com/Qianshunan/x264.git"
 $source_path = "$repos_path/x264/"
 $install_path = "$libs_path/x264"
 Set-Location $source_path
@@ -16,7 +16,7 @@ set -e
 cd $(cygpath.exe $source_path)
 
 ./configure \
---prefix="$(cygpath.exe ${install_path})" \
+--prefix="$(cygpath.exe $install_path)" \
 --enable-shared \
 --disable-opencl \
 --enable-pic
@@ -28,5 +28,5 @@ make install
 
 Write-Host "`n`n`n========================================"
 Write-Host "pc 文件的内容："
-Get-Content $install_path/lib/pkgconfig/x264.pc
+Get-Content "$install_path/lib/pkgconfig/x264.pc"
 Pop-Location
