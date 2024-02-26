990ac36d335959f10fda838b3816d1c4919c32d5
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 09:45:55 +0800

    build-nlohmann-json

---
+++ msys2/build-nlohmann-json.ps1
@@ -0,0 +1,16 @@
+param (
+	[string]$libs_path = $env:libs_path,
+	[string]$repos_path = $env:repos_path,
+	[string]$cpp_lib_build_scripts_path = $env:cpp_lib_build_scripts_path
+)
+$ErrorActionPreference = "Stop"
+
+$install_path = "$libs_path/nlohmann-json/include/nlohmann"
+
+if (Test-Path -Path "$install_path/json.hpp")
+{
+	Remove-Item -Path "$install_path/json.hpp" -Force
+}
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp `
+	-OutFile "$install_path/json.hpp"
