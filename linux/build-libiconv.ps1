80540236f753b85178a5d3f6c86301a9696d9b22
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 11:37:02 +0800

    删除Set-Location $repos_path

--- linux/build-libiconv.ps1
+++ linux/build-libiconv.ps1
@@ -5,7 +5,6 @@ param (
 )
 $ErrorActionPreference = "Stop"
 
-Set-Location $repos_path
 wget-repo.ps1 -workspace_dir $repos_path `
 	-repo_url https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz `
 	-out_dir_name libiconv
