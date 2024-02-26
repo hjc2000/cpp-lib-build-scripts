58b32847c1995ca1daf321445ce7ff22fcbe0017
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 14:36:38 +0800

    build-xz.ps1

--- msys2/build-xz.ps1
+++ msys2/build-xz.ps1
@@ -12,9 +12,9 @@ wget-repo.ps1 -workspace_dir $repos_path `
 $source_path = "$repos_path/xz/xz-5.4.6"
 $build_path = "$source_path/build"
 $install_path = "$libs_path/xz/"
+
 New-Item -ItemType Directory -Path $build_path -Force
 Remove-Item "$build_path/*" -Recurse -Force
-Set-Location $build_path
 
 # 创建文件 toolchain.cmake
 New-Item -ItemType File -Path "$build_path/toolchain.cmake" -Force
@@ -25,8 +25,9 @@ set(CMAKE_RC_COMPILER llvm-rc)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 "@
-$toolchain_file_content | Out-File -FilePath toolchain.cmake -Encoding UTF8
+$toolchain_file_content | Out-File -FilePath "$build_path/toolchain.cmake" -Encoding UTF8
 
+Set-Location $build_path
 cmake -G "Ninja" $source_path `
 	-DCMAKE_TOOLCHAIN_FILE="$build_path/toolchain.cmake" `
 	-DCMAKE_BUILD_TYPE=Release `
@@ -37,5 +38,4 @@ ninja install
 
 # 修复 .pc 文件内的路径
 update-pc-prefix.ps1 "${install_path}/lib/pkgconfig/liblzma.pc"
-Get-Content "${install_path}/lib/pkgconfig/liblzma.pc"
 Pop-Location
