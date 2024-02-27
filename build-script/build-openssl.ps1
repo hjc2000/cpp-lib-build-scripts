ed060c7da14fe2378265c9eb860140dcdc243dba
hjc2000 <huangjincanqqmail@qq.com>
2024-02-27 09:48:47 +0800

    $env:Path = "C:\msys64\usr\bin\core_perl;" + $env:Path

--- build-script/build-openssl.ps1
+++ build-script/build-openssl.ps1
@@ -4,6 +4,12 @@ param (
 	[string]$cpp_lib_build_scripts_path = $env:cpp_lib_build_scripts_path
 )
 $ErrorActionPreference = "Stop"
+if ($IsWindows)
+{
+	# 编译后安装阶段依赖 Perl 中的 pod2man，但是它比较不同寻常，
+	# 可执行文件不是在 bin 里面，而是在 bin 里面又创建了一个目录
+	$env:Path = "C:\msys64\usr\bin\core_perl;" + $env:Path
+}
 
 Push-Location $repos_path
 get-git-repo.ps1 -git_url "https://gitee.com/hughpenn23/openssl.git"
