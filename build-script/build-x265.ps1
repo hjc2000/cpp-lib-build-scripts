7e0646e877543ba6618624ee1deafd464a2eab6d
hjc2000 <huangjincanqqmail@qq.com>
2024-02-27 18:25:29 +0800

    build-x265.ps1

--- build-script/build-x265.ps1
+++ build-script/build-x265.ps1
@@ -15,8 +15,27 @@ $install_path = "$libs_path/x265/"
 New-Item -Path $build_path -ItemType Directory -Force
 Remove-Item "$build_path/*" -Recurse -Force
 
+# 创建文件 toolchain.cmake
+$toolchain_file_content = ""
+if ($cross_compile)
+{
+	$toolchain_file_content = @"
+set(CROSS_COMPILE_ARM 1)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR armv4)
+
+set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
+"@
+}
+
+New-Item -ItemType File -Path "$build_path/toolchain.cmake" -Force
+$toolchain_file_content | Out-File -FilePath "$build_path/toolchain.cmake" -Encoding UTF8
+
+# 切换到 build 目录开始构建
 Set-Location $build_path
 cmake -G "Unix Makefiles" $source_path `
+	-DCMAKE_TOOLCHAIN_FILE="$build_path/toolchain.cmake" `
 	-DCMAKE_INSTALL_PREFIX="${install_path}" `
 	-DENABLE_SHARED=on `
 	-DENABLE_PIC=on `
