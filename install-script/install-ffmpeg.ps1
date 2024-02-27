d63317a583d469cbcd1b85e12a534b43df732304
hjc2000 <huangjincanqqmail@qq.com>
2024-02-27 09:00:23 +0800

    $IsWindows

--- install-script/install-ffmpeg.ps1
+++ install-script/install-ffmpeg.ps1
@@ -1,11 +1,10 @@
+# 将环境变量 libs 目录中的库安装到特定的目录。
+# 安装到的目录取决于系统。在 windows 中，安装到 $env:install，
+# 在 linux 中，安装到 /usr/
 $libs_to_install = @(
-	"ffmpeg",
-	"x264",
-	"x265"
-	"SDL2",
-	"openssl"
+	"ffmpeg", "x264", "x265", "SDL2", "openssl"
 )
-if (is-msys.ps1)
+if ($IsWindows)
 {
 	if (-not $env:install)
 	{
