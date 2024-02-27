b82ae57d30ca389822e693baf3743b1bc41699f9
hjc2000 <huangjincanqqmail@qq.com>
2024-02-27 10:30:11 +0800

    sudo pwsh -File install-lib.ps1 -src_path "$env:libs_path/$lib" -dst_path "/usr/"

--- install-script/install-ffmpeg.ps1
+++ install-script/install-ffmpeg.ps1
@@ -21,8 +21,6 @@ else
 	# linux 平台
 	foreach ($lib in $libs_to_install)
 	{
-		sudo su
-		install-lib.ps1 -src_path "$env:libs_path/$lib" -dst_path "/usr/"
-		logout
+		sudo pwsh -File install-lib.ps1 -src_path "$env:libs_path/$lib" -dst_path "/usr/"
 	}
 }
