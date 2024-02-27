e1b2ca8c1faf66d8603e106186713bcb8395763a
hjc2000 <huangjincanqqmail@qq.com>
2024-02-27 09:19:38 +0800

    build-sdl2.ps1

--- build-script/build-sdl2.ps1
+++ build-script/build-sdl2.ps1
@@ -5,6 +5,23 @@ param (
 )
 $ErrorActionPreference = "Stop"
 
+if ($IsLinux)
+{
+	# 通过 apt 安装依赖
+	$dependent_libs = @(
+		"libasound2-dev",
+		"libpulse-dev"
+	)
+	foreach ($lib in $dependent_libs)
+	{
+		if (-not (apt list --installed | grep "$lib"))
+		{
+			# 如果此包没安装，安装。
+			sudo apt install $lib
+		}
+	}
+}
+
 Push-Location $repos_path
 get-git-repo.ps1 -git_url https://github.com/libsdl-org/SDL.git `
 	-branch_name release-2.30.x
@@ -16,14 +33,19 @@ New-Item -Path $build_path -ItemType Directory -Force
 Remove-Item -Path "$build_path/*" -Recurse -Force
 
 # 创建文件 toolchain.cmake
-New-Item -ItemType File -Path "$build_path/toolchain.cmake" -Force
-$toolchain_file_content = @"
+$toolchain_file_content = ""
+if ($IsWindows)
+{
+	$toolchain_file_content = @"
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x64)
 set(CMAKE_RC_COMPILER llvm-rc)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 "@
+}
+
+New-Item -ItemType File -Path "$build_path/toolchain.cmake" -Force
 $toolchain_file_content | Out-File -FilePath "$build_path/toolchain.cmake" -Encoding UTF8
 
 Set-Location $build_path
@@ -37,11 +59,14 @@ cmake -G "Ninja" $source_path `
 ninja -j12
 ninja install
 
-# 修复 .pc 文件内的路径
-update-pc-prefix.ps1 "${install_path}/lib/pkgconfig/sdl2.pc"
-Write-Host "`n`n`n========================================"
-Write-Host "pc 文件的内容："
-Get-Content "${install_path}/lib/pkgconfig/sdl2.pc"
+if ($IsWindows)
+{
+	# 修复 .pc 文件内的路径
+	update-pc-prefix.ps1 "${install_path}/lib/pkgconfig/sdl2.pc"
+	Write-Host "`n`n`n========================================"
+	Write-Host "pc 文件的内容："
+	Get-Content "${install_path}/lib/pkgconfig/sdl2.pc"
+}
 
 # 将头文件移出来，不然它是处于 include/SDL2/ 内
 Move-Item -Path "${install_path}/include/SDL2/*" `
