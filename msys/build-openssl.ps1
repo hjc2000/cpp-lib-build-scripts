e65650391e4ab8ede9a0b19e2deea5d2d4b2af6e
hjc2000 <huangjincanqqmail@qq.com>
2024-02-28 00:10:30 +0800

    1

--- msys/build-openssl.ps1
+++ msys/build-openssl.ps1
@@ -8,22 +8,16 @@ $ErrorActionPreference = "Stop"
 $source_path = "$repos_path/openssl"
 $install_path = "$libs_path/openssl"
 $build_path = "$source_path/build" # cmake 项目才需要
-
-
-
-
-# 编译后安装阶段依赖 Perl 中的 pod2man，但是它比较不同寻常，
-# 可执行文件不是在 bin 里面，而是在 bin 里面又创建了一个目录
-$env:Path = "C:\msys64\usr\bin\core_perl;" + $env:Path
-
-
-
 Push-Location $repos_path
-get-git-repo.ps1 -git_url "https://gitee.com/hughpenn23/openssl.git"
+try
+{
+	get-git-repo.ps1 -git_url "https://gitee.com/hughpenn23/openssl.git"
 
-
-
-run-bash-cmd.ps1 @"
+	# 编译后安装阶段依赖 Perl 中的 pod2man，但是它比较不同寻常，
+	# 可执行文件不是在 bin 里面，而是在 bin 里面又创建了一个目录
+	$env:Path = "C:\msys64\usr\bin\core_perl;" + $env:Path
+	
+	run-bash-cmd.ps1 @"
 set -e
 cd $(cygpath.exe $source_path)
 
@@ -33,5 +27,12 @@ cd $(cygpath.exe $source_path)
 make -j12
 make install
 "@
-
-Pop-Location
+}
+catch
+{
+	<#Do this if a terminating exception happens#>
+}
+finally
+{
+	Pop-Location
+}
