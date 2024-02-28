6d57ea58da62b97a0459e733c926d352366d5f71
hjc2000 <huangjincanqqmail@qq.com>
2024-02-28 22:13:22 +0800

    Install-Lib

--- base-script/import-functions.ps1
+++ base-script/import-functions.ps1
@@ -69,3 +69,73 @@ function Fix-PC-Config-PC-File
 	Write-Host "Updated prefix in $path_to_pc_file to $new_prefix_path"
 	Get-Content $path_to_pc_file
 }
+
+# 安装一个库。
+# 所谓的库，就是内含 lib, bin, include 等目录的一个目录。
+# 安装就是将库的这些目录复制到指定的位置。在 linux 中复制时会保留符号链接。
+function Install-Lib
+{
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$src_path, # 库的路径
+
+		[Parameter(Mandatory = $true)]
+		[string]$dst_path, # 要将库安装到哪里
+
+		[switch]$sudo # 是否用 sudo 执行复制。只有在 linux 中才有效。
+	)
+	$ErrorActionPreference = "Stop"
+
+	Write-Host "将 $src_path 安装到 $dst_path"
+
+	New-Item -Path $dst_path/bin/ -ItemType Directory -Force
+	New-Item -Path $dst_path/lib/ -ItemType Directory -Force
+	New-Item -Path $dst_path/include/ -ItemType Directory -Force
+
+	if ($IsWindows)
+	{
+		Copy-Item -Path "$src_path/bin/*" -Destination "$dst_path/bin/" -Force -Recurse
+		if (Test-Path "$src_path/lib/*")
+		{
+			Copy-Item -Path "$src_path/lib/*" -Destination "$dst_path/lib/" -Force -Recurse
+		}
+		elseif (Test-Path -Path "$src_path/lib64")
+		{
+			Copy-Item -Path "$src_path/lib64/*" -Destination "$dst_path/lib/" -Force -Recurse
+		}
+		elseif (Test-Path -Path "$src_path/lib32")
+		{
+			Copy-Item -Path "$src_path/lib32/*" -Destination "$dst_path/lib/" -Force -Recurse
+		}
+
+
+		Copy-Item -Path "$src_path/include/*" -Destination "$dst_path/include/" -Force -Recurse
+	}
+	else
+	{
+		# linux 平台
+		$copy_cmd = "cp -a -r"
+		if ($sudo)
+		{
+			$copy_cmd = "sudo cp -a -r"
+		}
+
+		# 下面是 bash 脚本
+		run-bash-cmd.ps1 @"
+set -e
+
+# 复制并保留符号链接
+$copy_cmd $src_path/bin/* $dst_path/bin/
+
+if [ -d "$src_path/lib" ]; then
+	$copy_cmd "$src_path/lib/"* "$dst_path/lib/"
+elif [ -d "$src_path/lib64" ]; then
+	$copy_cmd "$src_path/lib64/"* "$dst_path/lib/"
+elif [ -d "$src_path/lib32" ]; then
+	$copy_cmd "$src_path/lib32/"* "$dst_path/lib/"
+fi
+
+$copy_cmd $src_path/include/* $dst_path/include/
+"@
+	}
+}
