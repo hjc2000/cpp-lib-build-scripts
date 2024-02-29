762ce3c77272227a2cc44ddd8d6a82819ebcdd6d
hjc2000 <huangjincanqqmail@qq.com>
2024-02-29 16:17:26 +0800

    auto commit

--- linux/build-pulse-audio.ps1
+++ linux/build-pulse-audio.ps1
@@ -13,7 +13,7 @@ try
 
 	# 构建依赖项
 	& $build_script_path/build-libsndfile.ps1
-	& $build_script_path/build-ltdl.ps1
+	& $build_script_path/build-libtool.ps1
 
 	$env:PKG_CONFIG_PATH = 
 	"$libs_path/libsndfile/lib/pkgconfig:"
