5e59ac2d86ebe849e7a15486858cce41e9416ec7
hjc2000 <huangjincanqqmail@qq.com>
2024-02-29 15:02:03 +0800

    1

--- linux/total-install-to-usr.ps1
+++ linux/total-install-to-usr.ps1
@@ -4,8 +4,10 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 Push-Location
 try
 {
+	Write-Host "首先安装到 .total-install"
 	& $build_script_path/total-install.ps1
-	
+
+	Write-Host "接着安装到 /usr/"
 	run-bash-cmd.ps1 @"
 	sudo cp -a $build_script_path/.total-install/ /usr/
 "@
