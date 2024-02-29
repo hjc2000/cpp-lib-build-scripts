f069dba371bdf460f89df169e40569b30176b545
hjc2000 <huangjincanqqmail@qq.com>
2024-02-29 16:18:24 +0800

    auto commit

--- linux/build-libtool.ps1
+++ linux/build-libtool.ps1
@@ -1,7 +1,7 @@
 $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../base-script/prepare-for-building.ps1
 
-$source_path = "$repos_path/libtool/"
+$source_path = "$repos_path/libtool/libtool-2.4.7"
 $install_path = "$libs_path/libtool/"
 $build_path = "$source_path/build/"
 Push-Location $repos_path
