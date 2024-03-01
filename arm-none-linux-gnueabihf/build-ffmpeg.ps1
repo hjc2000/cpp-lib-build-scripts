98d157ab1cdb1d092236d95ee4355cad6f4e44f9
hjc2000 <huangjincanqqmail@qq.com>
2024-03-02 00:34:52 +0800

    1

--- arm-none-linux-gnueabihf/build-ffmpeg.ps1
+++ arm-none-linux-gnueabihf/build-ffmpeg.ps1
@@ -6,11 +6,11 @@ $install_path = "$libs_path/ffmpeg/"
 Push-Location $repos_path
 try
 {
-	Import-Lib -LibName "x264" -NotBuild
-	Import-Lib -LibName "x265" -NotBuild
-	Import-Lib -LibName "sdl2" -NotBuild
-	Import-Lib -LibName "amf" -NotBuild
-	Import-Lib -LibName "openssl" -NotBuild
+	Import-Lib -LibName "x264"
+	Import-Lib -LibName "x265"
+	Import-Lib -LibName "sdl2"
+	Import-Lib -LibName "amf"
+	Import-Lib -LibName "openssl"
 	Write-Host $env:PKG_CONFIG_PATH
 
 	Set-Location $repos_path
