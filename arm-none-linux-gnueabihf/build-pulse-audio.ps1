cbc7adcfc9dcab07dece526938f665dd580e0315
hjc2000 <huangjincanqqmail@qq.com>
2024-03-01 15:00:07 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-pulse-audio.ps1
+++ arm-none-linux-gnueabihf/build-pulse-audio.ps1
@@ -30,9 +30,6 @@ try
 	strip = 'arm-none-linux-gnueabihf-strip'
 	pkg-config = 'pkg-config'
 
-	[properties]
-	sys_root='$total_install_path'
-
 	[host_machine]
 	system = 'linux'
 	cpu_family = 'arm'
