20cf7f300511ed91b3c314b31ad1887468b9f309
hjc2000 <huangjincanqqmail@qq.com>
2024-03-01 15:22:14 +0800

    auto commit

---
+++ arm-none-linux-gnueabihf/build-sdl2.ps1
@@ -0,0 +1,57 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+
+$source_path = "$repos_path/SDL/"
+$install_path = "$libs_path/SDL2/"
+$build_path = "$source_path/build/"
+Push-Location $repos_path
+try
+{
+	# 构建依赖项
+	& $build_script_path/build-pulse-audio.ps1
+	Clear-Pkg-Config-Path
+	Append-Pkg-Config-Path -Path "$libs_path/build-pulse-audio/lib/pkgconfig"
+
+
+	
+	get-git-repo.ps1 -git_url https://gitee.com/mycn027b/SDL.git `
+		-branch_name release-2.30.x
+
+	New-Empty-Dir $build_path
+	Create-Text-File -Path "$build_path/toolchain.cmake" `
+		-Content @"
+	set(CROSS_COMPILE_ARM 1)
+	set(CMAKE_SYSTEM_NAME Linux)
+	set(CMAKE_SYSTEM_PROCESSOR armv7-a)
+
+	set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)
+	set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
+"@
+	
+	Set-Location $build_path
+	cmake -G "Ninja" $source_path `
+		-DCMAKE_BUILD_TYPE=Release `
+		-DCMAKE_INSTALL_PREFIX="$install_path" `
+		-DSDL_SHARED=ON `
+		-DSDL_STATIC=OFF `
+		-DSDL_WAYLAND=OFF `
+		-DSDL_IBUS=OFF
+		
+
+	ninja -j12
+	ninja install
+
+	# 将头文件移出来，不然它是处于 include/SDL2/ 内
+	Move-Item -Path "$install_path/include/SDL2/*" `
+		-Destination "$install_path/include/" `
+		-Force
+	Remove-Item "$install_path/include/SDL2/"
+}
+catch
+{
+	throw
+}
+finally
+{
+	Pop-Location
+}
