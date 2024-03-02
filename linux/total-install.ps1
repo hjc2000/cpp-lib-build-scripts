0bc5e78cabfe3b1575d47c0128d6e67dcf32cc72
hjc2000 <huangjincanqqmail@qq.com>
2024-03-02 16:14:58 +0800

    stow -t "$HOME/install" $(Split-Path $lib -Leaf)

--- linux/total-install.ps1
+++ linux/total-install.ps1
@@ -12,7 +12,7 @@ try
 	$libs = Get-ChildItem -Path $build_script_path/.libs/ -Directory
 	foreach ($lib in $libs)
 	{
-		Install-Lib -src_path $lib -dst_path $build_script_path/.total-install/
+		stow -t "$HOME/install" $(Split-Path $lib -Leaf)
 	}
 }
 catch
