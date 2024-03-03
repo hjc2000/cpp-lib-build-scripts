03e8ab98c3e379b17e59202fad1718bef9550999
hjc2000 <huangjincanqqmail@qq.com>
2024-03-03 14:57:41 +0800

    total-install.ps1

--- arm-none-linux-gnueabihf/total-install.ps1
+++ arm-none-linux-gnueabihf/total-install.ps1
@@ -4,16 +4,13 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 Push-Location
 try
 {
-	$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-	. $build_script_path/../.base-script/import-functions.ps1
-
-	New-Item -Path "$build_script_path/.total-install/" -ItemType Directory -Force
+	New-Item -Path $total_install_path -ItemType Directory -Force
 
 	# 使用Get-ChildItem获取所有子目录，-Directory参数确保只获取目录
-	$libs = Get-ChildItem -Path $build_script_path/.libs/ -Directory
+	$libs = Get-ChildItem -Path $libs_path -Directory
 	foreach ($lib in $libs)
 	{
-		Install-Lib -src_path $lib -dst_path $build_script_path/.total-install/
+		Install-Lib -src_path $lib -dst_path $total_install_path
 	}
 }
 catch
