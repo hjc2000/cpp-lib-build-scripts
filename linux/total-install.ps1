26d665a670d8dfb530c4a38193037fec0e06cd56
hjc2000 <huangjincanqqmail@qq.com>
2024-03-03 14:48:21 +0800

    stow -t "$HOME/install" ".total-install"

--- linux/total-install.ps1
+++ linux/total-install.ps1
@@ -12,12 +12,13 @@ try
 
 	# 使用Get-ChildItem获取所有子目录，-Directory参数确保只获取目录
 	$libs = Get-ChildItem -Path $build_script_path/.libs/ -Directory
-	Set-Location "$build_script_path/.libs/"
 	foreach ($lib in $libs)
 	{
-		stow -t "$HOME/install" $(Split-Path $lib -Leaf)
-		Install-Lib -src_path $lib -dst_path $build_script_path/.total-install/
+		Install-Lib -src_path $lib -dst_path "$build_script_path/.total-install/"
 	}
+
+	Set-Location $build_script_path
+	stow -t "$HOME/install" ".total-install"
 }
 catch
 {
