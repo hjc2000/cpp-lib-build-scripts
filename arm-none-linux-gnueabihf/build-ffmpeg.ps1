d8e67bcc18e7f3416930fa191f748546897d1437
hjc2000 <huangjincanqqmail@qq.com>
2024-03-03 22:09:03 +0800

    --enable-sdl \

--- arm-none-linux-gnueabihf/build-ffmpeg.ps1
+++ arm-none-linux-gnueabihf/build-ffmpeg.ps1
@@ -10,11 +10,13 @@ try
 	& "${build_script_path}/build-x264.ps1"
 	& "${build_script_path}/build-x265.ps1"
 	& "${build_script_path}/build-openssl.ps1"
+	& "${build_script_path}/build-sdl2.ps1"
 	# 设置依赖项的 pkg-config
 	Clear-PkgConfig-Path
 	Append-Pkg-Config-Path-Recurse -Path "$libs_path/x264"
 	Append-Pkg-Config-Path-Recurse -Path "$libs_path/x265"
 	Append-Pkg-Config-Path-Recurse -Path "$libs_path/openssl"
+	Append-Pkg-Config-Path-Recurse -Path "$libs_path/sdl2"
 	Write-Host "PKG_CONFIG_PATH 的值：$env:PKG_CONFIG_PATH"
 
 
@@ -31,6 +33,7 @@ try
 	--enable-libx264 \
 	--enable-libx265 \
 	--enable-openssl \
+	--enable-sdl \
 	--enable-version3 \
 	--enable-pic \
 	--enable-gpl \
