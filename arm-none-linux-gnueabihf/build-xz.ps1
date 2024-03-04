d34b352bb8ee39e7639dccfea63dceed94e0566f
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 14:19:44 +0800

    build-xz

---
+++ arm-none-linux-gnueabihf/build-xz.ps1
@@ -0,0 +1,48 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+
+$source_path = "$repos_path/xz/"
+$install_path = "$libs_path/xz/"
+$build_path = "$source_path/build/"
+Push-Location $repos_path
+try
+{
+	get-git-repo.ps1 -git_url "https://github.com/tukaani-project/xz.git"
+
+	New-Empty-Dir $build_path
+	Create-Text-File -Path "$build_path/toolchain.cmake" `
+		-Content @"
+	set(CROSS_COMPILE_ARM 1)
+	set(CMAKE_SYSTEM_NAME Linux)
+	set(CMAKE_SYSTEM_PROCESSOR armv7-a)
+
+	set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)
+	set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
+
+	# 指定查找程序、库、头文件时的根路径，防止在默认系统路径中查找
+	set(CMAKE_FIND_ROOT_PATH "$total_install_path")
+	# 设置查找路径的模式，确保仅在指定的根路径中查找
+	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+"@
+	
+
+	Set-Location $build_path
+	cmake -G "Ninja" $source_path `
+		-DCMAKE_TOOLCHAIN_FILE="$build_path/toolchain.cmake" `
+		-DCMAKE_BUILD_TYPE=Release `
+		-DCMAKE_INSTALL_PREFIX="$install_path"
+
+	ninja -j12
+	ninja install
+}
+catch
+{
+	throw
+}
+finally
+{
+	Pop-Location
+}
