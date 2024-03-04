af658ca9dd04fc10eefb0926f9974e4f4f3e3e67
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 14:46:34 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-libxml2.ps1
+++ arm-none-linux-gnueabihf/build-libxml2.ps1
@@ -33,7 +33,6 @@ try
 	set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
 
 	# 指定查找程序、库、头文件时的根路径，防止在默认系统路径中查找
-	set(CMAKE_PREFIX_PATH "$total_install_path/lib")
 	set(CMAKE_FIND_ROOT_PATH "$total_install_path")
 	# 设置查找路径的模式，确保仅在指定的根路径中查找
 	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -49,10 +48,9 @@ try
 		-DCMAKE_BUILD_TYPE=Release `
 		-DCMAKE_INSTALL_PREFIX="$install_path" `
 		-DLIBXML2_WITH_PYTHON=OFF `
-		-DLIBXML2_WITH_TESTS=OFF
-	#		-DLIBXML2_WITH_PROGRAMS=OFF
-	#		-DLIBXML2_WITH_LZMA=OFF `
-
+		-DLIBXML2_WITH_TESTS=OFF `
+		-DLIBXML2_WITH_CATALOG=OFF `
+		-DLIBXML2_WITH_DEBUG=OFF
 
 	ninja -j12
 	ninja install
