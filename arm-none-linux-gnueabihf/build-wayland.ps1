2682ffff4c5c21c0dc42f4123d6bd27889cb3bd8
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 14:03:57 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-wayland.ps1
+++ arm-none-linux-gnueabihf/build-wayland.ps1
@@ -11,9 +11,11 @@ try
 
 	# 构建依赖项
 	& "${build_script_path}/build-libffi.ps1"
+	& "${build_script_path}/build-libxml2.ps1"
 	# 设置依赖项的 pkg-config
 	Clear-PkgConfig-Path
 	Append-Pkg-Config-Path-Recurse -Path "$libs_path/libffi"
+	Append-Pkg-Config-Path-Recurse -Path "$libs_path/libxml2"
 	Write-Host "PKG_CONFIG_PATH 的值：$env:PKG_CONFIG_PATH"
 	Total-Install
 	
