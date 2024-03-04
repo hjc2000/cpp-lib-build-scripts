88b4b7590616a3a09729795b9707299b61b32565
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 14:40:50 +0800

    set(CMAKE_PREFIX_PATH "$total_install_path")

--- arm-none-linux-gnueabihf/build-libxml2.ps1
+++ arm-none-linux-gnueabihf/build-libxml2.ps1
@@ -33,12 +33,13 @@ try
 	set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
 
 	# 指定查找程序、库、头文件时的根路径，防止在默认系统路径中查找
+	set(CMAKE_PREFIX_PATH "$total_install_path")
 	set(CMAKE_FIND_ROOT_PATH "$total_install_path")
 	# 设置查找路径的模式，确保仅在指定的根路径中查找
 	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
-	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-	set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+	set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 "@
 	
 
