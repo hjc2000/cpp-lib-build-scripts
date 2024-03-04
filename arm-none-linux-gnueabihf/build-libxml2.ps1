afadef11ee4439adfb03ed17d192011c5fadc588
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 21:51:07 +0800

    include_directories("$total_install_path/include") 	link_directories("$total_install_path/lib")

--- arm-none-linux-gnueabihf/build-libxml2.ps1
+++ arm-none-linux-gnueabihf/build-libxml2.ps1
@@ -39,8 +39,10 @@ try
 	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 	set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-	link_libraries("$total_install_path/lib/liblzma.so.5")
-	link_libraries("$total_install_path/lib/libiconv.so.2")
+	include_directories("$total_install_path/include")
+	link_directories("$total_install_path/lib")
+	# link_libraries("$total_install_path/lib/liblzma.so.5")
+	# link_libraries("$total_install_path/lib/libiconv.so.2")
 "@
 	
 
