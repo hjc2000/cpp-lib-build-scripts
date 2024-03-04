acba597fc9556ceac437cbc12484fd7091515f11
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 23:29:29 +0800

    librt.so.1

--- arm-none-linux-gnueabihf/build-wayland.ps1
+++ arm-none-linux-gnueabihf/build-wayland.ps1
@@ -56,7 +56,7 @@ try
 	[built-in options]
 	c_args = ['-I$total_install_path/include']
 	cpp_args = ['-I$total_install_path/include']
-	c_link_args = ['-L$total_install_path/lib', '$total_install_path/lib/libxml2.so.2', '$total_install_path/lib/liblzma.so.5']
+	c_link_args = ['-L$total_install_path/lib', 'librt.so.1' , '$total_install_path/lib/libxml2.so.2', '$total_install_path/lib/liblzma.so.5']
 	cpp_link_args = ['-L$total_install_path/lib']
 "@
 
