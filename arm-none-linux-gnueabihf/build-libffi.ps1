0853052f3a309117e1e085518234b27106bd76ab
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 13:21:02 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-libffi.ps1
+++ arm-none-linux-gnueabihf/build-libffi.ps1
@@ -1,27 +1,20 @@
 $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 
-$source_path = "$repos_path/libffi/"
+$source_path = "$repos_path/libffi/libffi-3.4.6"
 $install_path = "$libs_path/libffi/"
 $build_path = "$source_path/build/"
 Push-Location $repos_path
 
 try
 {
-	Apt-Ensure-Packets @(
-		"autoconf",
-		"automake",
-		"libtool"
-	)
-
 	Set-Location $repos_path
-	get-git-repo.ps1 -git_url https://github.com/libffi/libffi.git
+	wget-repo.ps1 -workspace_dir $repos_path `
+		-repo_url "https://github.com/libffi/libffi/releases/download/v3.4.6/libffi-3.4.6.tar.gz" `
+		-out_dir_name "libffi"
 
 	run-bash-cmd.ps1 @"
 	cd $source_path
-	aclocal
-	autoconf
-	autoreconf -i
 
 	./configure \
 	--prefix=$install_path \
