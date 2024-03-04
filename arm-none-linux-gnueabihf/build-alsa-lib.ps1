cc501419465dee760da01735f11f24513e1185ee
hjc2000 <huangjincanqqmail@qq.com>
2024-03-04 09:03:02 +0800

    $source_path = "$repos_path/alsa-lib/alsa-lib-1.2.7.2"

--- arm-none-linux-gnueabihf/build-alsa-lib.ps1
+++ arm-none-linux-gnueabihf/build-alsa-lib.ps1
@@ -1,7 +1,7 @@
 $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 
-$source_path = "$repos_path/alsa-lib/"
+$source_path = "$repos_path/alsa-lib/alsa-lib-1.2.7.2"
 $install_path = "$libs_path/alsa-lib/"
 $build_path = "$source_path/build/"
 Push-Location $repos_path
