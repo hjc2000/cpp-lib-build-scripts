186aa5aa19e2e39b0dacc9427d8f8dbb93f08f8c
hjc2000 <huangjincanqqmail@qq.com>
2024-03-05 23:58:13 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-libxml2.ps1
+++ arm-none-linux-gnueabihf/build-libxml2.ps1
@@ -42,11 +42,12 @@ try
 	set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 	include_directories("$total_install_path/include")
-	link_libraries(
-		"$total_install_path/lib/liblzma.so.5"
-		"$total_install_path/lib/libiconv.so.2"
-		"$total_install_path/lib/libz.so.1"
-	)
+	link_directories("lib")
+	# link_libraries(
+	# 	"$total_install_path/lib/liblzma.so.5"
+	# 	"$total_install_path/lib/libiconv.so.2"
+	# 	"$total_install_path/lib/libz.so.1"
+	# )
 "@
 	
 
