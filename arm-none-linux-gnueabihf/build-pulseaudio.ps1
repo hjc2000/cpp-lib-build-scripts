35a23edd028627888c43b76e3aa7601c9899ba0c
hjc2000 <huangjincanqqmail@qq.com>
2024-03-05 10:08:44 +0800

    armv4

--- arm-none-linux-gnueabihf/build-pulseaudio.ps1
+++ arm-none-linux-gnueabihf/build-pulseaudio.ps1
@@ -47,18 +47,18 @@ try
 	[host_machine]
 	system = 'linux'
 	cpu_family = 'arm'
-	cpu = 'armv7-a'
+	cpu = 'armv4'
 	endian = 'little'
 
 	[target_machine]
 	system = 'linux'
 	cpu_family = 'arm'
-	cpu = 'armv7-a'
+	cpu = 'armv4'
 	endian = 'little'
 
 	[built-in options]
-	c_args = ['-march=armv7-a', '-I$total_install_path/include']
-	cpp_args = ['-march=armv7-a', '-I$total_install_path/include']
+	c_args = ['-march=armv4', '-I$total_install_path/include']
+	cpp_args = ['-march=armv4', '-I$total_install_path/include']
 	c_link_args = ['-L$total_install_path/lib', '$total_install_path/lib/libltdl.so.7']
 	cpp_link_args = ['-L$total_install_path/lib']
 "@
