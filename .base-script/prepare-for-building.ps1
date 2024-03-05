90b89db9f80c257bf53cf38899e423902b229dd8
hjc2000 <huangjincanqqmail@qq.com>
2024-03-05 20:07:42 +0800

    | Out-Null

--- .base-script/prepare-for-building.ps1
+++ .base-script/prepare-for-building.ps1
@@ -10,6 +10,6 @@ $repos_path = "$build_script_path/.repos"
 $libs_path = "$build_script_path/.libs"
 $total_install_path = "$build_script_path/.total-install"
 
-New-Item -Path $repos_path -ItemType Directory -Force
-New-Item -Path $libs_path -ItemType Directory -Force
-New-Item -Path $total_install_path -ItemType Directory -Force
+New-Item -Path $repos_path -ItemType Directory -Force | Out-Null
+New-Item -Path $libs_path -ItemType Directory -Force | Out-Null
+New-Item -Path $total_install_path -ItemType Directory -Force | Out-Null
