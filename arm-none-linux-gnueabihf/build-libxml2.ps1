832eb94697c383e4b627725a6065489030e99bff
hjc2000 <huangjincanqqmail@qq.com>
2024-03-06 00:15:25 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-libxml2.ps1
+++ arm-none-linux-gnueabihf/build-libxml2.ps1
@@ -11,12 +11,6 @@ try
 	& "${build_script_path}/build-libiconv.ps1"
 	& "${build_script_path}/build-xz.ps1"
 	& "${build_script_path}/build-zlib.ps1"
-	# 设置依赖项的 pkg-config
-	Clear-PkgConfig-Path
-	# Append-Pkg-Config-Path-Recurse -Path "$libs_path/libiconv"
-	# Append-Pkg-Config-Path-Recurse -Path "$libs_path/xz"
-	# Append-Pkg-Config-Path-Recurse -Path "$libs_path/zlib"
-	Write-Host "PKG_CONFIG_PATH 的值：$env:PKG_CONFIG_PATH"
 	Total-Install
 
 
