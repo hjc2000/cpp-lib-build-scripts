0593e68a4b38736122d229f6aa70f05a351045b7
hjc2000 <huangjincanqqmail@qq.com>
2024-03-05 14:14:32 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-wayland.ps1
+++ arm-none-linux-gnueabihf/build-wayland.ps1
@@ -45,6 +45,7 @@ try
 "@
 	$c_link_args = $c_link_args.Replace("\r", " ")
 	$c_link_args = $c_link_args.Replace("\n", " ")
+	$c_link_args = $c_link_args.Replace("\t", " ")
 	$c_link_args = $c_link_args.Trim()
 
 	Create-Text-File -Path $build_path/cross_file.ini `
