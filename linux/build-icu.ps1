eddea96e6461eb0c4907261cd6722c1bc09491d7
hjc2000 <huangjincanqqmail@qq.com>
2024-03-05 15:46:17 +0800

    build-icu

---
+++ linux/build-icu.ps1
@@ -0,0 +1,33 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+
+$source_path = "$repos_path/icu/icu4c/source/"
+$install_path = "$libs_path/icu"
+Push-Location $repos_path
+try
+{
+	get-git-repo.ps1 -git_url "https://github.com/unicode-org/icu.git"
+
+	# 执行命令进行构建
+	run-bash-cmd.ps1 @"
+	set -e
+	cd $source_path
+
+	touch configure-help.txt
+	./configure -h > configure-help.txt
+
+	./configure \
+	--prefix="$install_path"
+
+	make -j12
+	make install
+"@
+}
+catch
+{
+	throw
+}
+finally
+{
+	Pop-Location
+}
