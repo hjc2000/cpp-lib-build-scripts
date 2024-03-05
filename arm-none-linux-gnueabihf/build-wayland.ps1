450d83bb2b1a1e768bf4e7af6acf8456c1dc5194
hjc2000 <huangjincanqqmail@qq.com>
2024-03-06 00:28:06 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-wayland.ps1
+++ arm-none-linux-gnueabihf/build-wayland.ps1
@@ -45,7 +45,7 @@ try
 		'$total_install_path/lib/libiconv.so.2',
 		'$total_install_path/lib/libxml2.so.2',
 		'$total_install_path/lib/liblzma.so.5',
-		'-llibexpat',
+		'$total_install_path/lib/libexpat.so.1',
 	]
 "@.Replace("`r", " ").Replace("`n", " ").Replace("`t", " ")
 
