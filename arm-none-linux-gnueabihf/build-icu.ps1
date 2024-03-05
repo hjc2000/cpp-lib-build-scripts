96108bb4c3eeec8cc23b134b62abb66208197e97
hjc2000 <huangjincanqqmail@qq.com>
2024-03-05 15:33:37 +0800

    build-icu

---
+++ arm-none-linux-gnueabihf/build-icu.ps1
@@ -0,0 +1,39 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+
+$source_path = "$repos_path/icu/icu4c/source/"
+$install_path = "$libs_path/icu"
+Push-Location $repos_path
+try
+{
+	get-git-repo.ps1 -git_url "https://github.com/unicode-org/icu.git"
+
+	# 执行命令进行构建
+	run-bash-cmd.ps1 @"
+	set -e
+	cd $source_path
+
+	./configure -h
+	exit
+
+	./configure \
+	--prefix="$install_path" \
+	--enable-shared \
+	--disable-opencl \
+	--enable-pic \
+	--host=arm-none-linux-gnueabihf \
+	--cross-prefix=arm-none-linux-gnueabihf-
+
+	make clean
+	make -j12
+	make install
+"@
+}
+catch
+{
+	throw
+}
+finally
+{
+	Pop-Location
+}
