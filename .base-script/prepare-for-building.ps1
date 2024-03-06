02f48e9ac8d499ecdec56b9df7c012ed1bd4aac4
hjc2000 <huangjincanqqmail@qq.com>
2024-03-06 16:24:07 +0800

    auto commit

--- .base-script/prepare-for-building.ps1
+++ .base-script/prepare-for-building.ps1
@@ -11,7 +11,6 @@ $libs_path = "$build_script_path/.libs"
 $total_install_path = "$build_script_path/.total-install"
 
 $env:PKG_CONFIG_PATH = "$total_install_path/lib/pkgconfig"
-$env:LIBRARY_PATH = "$total_install_path/lib"
 
 New-Item -Path $repos_path -ItemType Directory -Force | Out-Null
 New-Item -Path $libs_path -ItemType Directory -Force | Out-Null
