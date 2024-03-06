b976c3c597010105cb343d0e6b18ed6f1c1fae64
hjc2000 <huangjincanqqmail@qq.com>
2024-03-06 08:41:50 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-cairo.ps1
+++ arm-none-linux-gnueabihf/build-cairo.ps1
@@ -10,10 +10,12 @@ try
 	# 构建依赖项
 	& "${build_script_path}/build-zlib.ps1"
 	& "${build_script_path}/build-libexpat.ps1"
+	& "${build_script_path}/build-glib.ps1"
 	# 设置依赖项的 pkg-config
 	Clear-PkgConfig-Path
 	Append-Pkg-Config-Path-Recurse -Path "$libs_path/zlib"
 	Append-Pkg-Config-Path-Recurse -Path "$libs_path/libexpat"
+	Append-Pkg-Config-Path-Recurse -Path "$libs_path/glib"
 	Write-Host "PKG_CONFIG_PATH 的值：$env:PKG_CONFIG_PATH"
 	Total-Install
 
---
+++ arm-none-linux-gnueabihf/build-libpng.ps1
@@ -0,0 +1,1 @@
+http://prdownloads.sourceforge.net/libpng/libpng-1.6.43.tar.gz?download
