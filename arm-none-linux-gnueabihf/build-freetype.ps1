d7f30c975503f3e7454b79b017bda8b6876f7616
hjc2000 <huangjincanqqmail@qq.com>
2024-03-07 00:21:08 +0800

    get-git-repo.ps1 -git_url "https://github.com/freetype/freetype.git"

--- arm-none-linux-gnueabihf/build-freetype.ps1
+++ arm-none-linux-gnueabihf/build-freetype.ps1
@@ -1,7 +1,7 @@
 $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 
-$source_path = "$repos_path/freetype/freetype-2.7"
+$source_path = "$repos_path/freetype/"
 $install_path = "$libs_path/freetype/"
 $build_path = "$source_path/build/"
 Push-Location $repos_path
@@ -10,9 +10,8 @@ try
 	& "${build_script_path}/build-zlib.ps1"
 	& "${build_script_path}/build-libpng.ps1"
 
-	wget-repo.ps1 -workspace_dir $repos_path `
-		-repo_url "https://download.savannah.gnu.org/releases/freetype/freetype-2.7.tar.gz" `
-		-out_dir_name "freetype"
+
+	get-git-repo.ps1 -git_url "https://github.com/freetype/freetype.git"
 
 	run-bash-cmd.ps1 @"
 	cd $source_path
