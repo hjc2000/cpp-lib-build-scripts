f7d4eae40d9c354c64422b8ae76073dcd55987de
hjc2000 <huangjincanqqmail@qq.com>
2024-03-06 16:34:02 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-glib.ps1
+++ arm-none-linux-gnueabihf/build-glib.ps1
@@ -19,7 +19,7 @@ try
 	# 开始构建本体
 	Set-Location $repos_path
 	get-git-repo.ps1 -git_url "https://gitlab.gnome.org/GNOME/glib.git" `
-		-branch_name "2.79.3"
+		-branch_name "2.78.4"
 
 	New-Item -Path $build_path -ItemType Directory -Force | Out-Null
 	Remove-Item "$build_path/*" -Recurse -Force
