37afc757d5cb71df284b554d384606c1c286e8d6
hjc2000 <huangjincanqqmail@qq.com>
2024-03-07 00:28:36 +0800

    build-freetype新版本使用梅森了

--- arm-none-linux-gnueabihf/build-freetype.ps1
+++ arm-none-linux-gnueabihf/build-freetype.ps1
@@ -7,23 +7,51 @@ $build_path = "$source_path/build/"
 Push-Location $repos_path
 try
 {
-	& "${build_script_path}/build-zlib.ps1"
-	& "${build_script_path}/build-libpng.ps1"
-
-
+	# 开始构建本体
+	Set-Location $repos_path
 	get-git-repo.ps1 -git_url "https://github.com/freetype/freetype.git"
 
-	run-bash-cmd.ps1 @"
-	cd $source_path
+	New-Item -Path $build_path -ItemType Directory -Force | Out-Null
+	Remove-Item "$build_path/*" -Recurse -Force
 
-	./configure \
-	--prefix="$install_path" \
-	--host=arm-none-linux-gnueabihf
+	Create-Text-File -Path $build_path/cross_file.ini `
+		-Content @"
+	[binaries]
+	c = 'arm-none-linux-gnueabihf-gcc'
+	cpp = 'arm-none-linux-gnueabihf-g++'
+	ar = 'arm-none-linux-gnueabihf-ar'
+	ld = 'arm-none-linux-gnueabihf-ld'
+	strip = 'arm-none-linux-gnueabihf-strip'
+	pkg-config = 'pkg-config'
 
-	make -j12
-	make install > /dev/null
+	[host_machine]
+	system = 'linux'
+	cpu_family = 'arm'
+	cpu = 'armv4'
+	endian = 'little'
+
+	[target_machine]
+	system = 'linux'
+	cpu_family = 'arm'
+	cpu = 'armv4'
+	endian = 'little'
+
+	[built-in options]
+	c_args = ['-march=armv4', '-I$total_install_path/include']
+	cpp_args = ['-march=armv4', '-I$total_install_path/include']
+	c_link_args = ['-L$total_install_path/lib/', ]
+	cpp_link_args = ['-L$total_install_path/lib/']
 "@
-	
+
+	Set-Location $source_path
+	meson setup build/ `
+		--prefix="$install_path" `
+		--cross-file="$build_path/cross_file.ini"
+
+	Set-Location $build_path
+	ninja -j12 | Out-Null
+	ninja install | Out-Null
+
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 }
 catch
