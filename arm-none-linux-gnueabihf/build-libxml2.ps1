e374aee93c73452479bf86948d3d32039b91412f
hjc2000 <huangjincanqqmail@qq.com>
2024-03-06 15:06:50 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-libxml2.ps1
+++ arm-none-linux-gnueabihf/build-libxml2.ps1
@@ -30,9 +30,9 @@ try
 	set(CMAKE_FIND_ROOT_PATH "$total_install_path")
 	# 设置查找路径的模式，确保仅在指定的根路径中查找
 	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
 	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-	set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+	set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 
 	include_directories("$total_install_path/include")
 	link_directories("$total_install_path/lib")
