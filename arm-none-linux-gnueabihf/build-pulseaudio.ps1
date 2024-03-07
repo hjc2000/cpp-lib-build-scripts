3c921d39d76558e0479d0609d0c244f63f9858f5
hjc2000 <huangjincanqqmail@qq.com>
2024-03-07 11:08:51 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-pulseaudio.ps1
+++ arm-none-linux-gnueabihf/build-pulseaudio.ps1
@@ -30,34 +30,12 @@ try
 	get-git-repo.ps1 -git_url "https://github.com/pulseaudio/pulseaudio.git"
 
 	New-Empty-Dir -Path $build_path
-	Create-Text-File -Path $build_path/cross_file.ini `
-		-Content @"
-	[binaries]
-	c = 'arm-none-linux-gnueabihf-gcc'
-	cpp = 'arm-none-linux-gnueabihf-g++'
-	ar = 'arm-none-linux-gnueabihf-ar'
-	ld = 'arm-none-linux-gnueabihf-ld'
-	strip = 'arm-none-linux-gnueabihf-strip'
-	pkg-config = 'pkg-config'
 
-	[host_machine]
-	system = 'linux'
-	cpu_family = 'arm'
-	cpu = 'armv4'
-	endian = 'little'
-
-	[target_machine]
-	system = 'linux'
-	cpu_family = 'arm'
-	cpu = 'armv4'
-	endian = 'little'
-
-	# 如果不设置低于 avmv6 的版本，编译时就会报内联汇编有不可能的约束。
-	[built-in options]
-	c_args = ['-march=armv4', '-I$total_install_path/include']
-	cpp_args = ['-march=armv4', '-I$total_install_path/include']
-	c_link_args = ['-L$total_install_path/lib', '$total_install_path/lib/libgdbm.so.6', '$total_install_path/lib/libltdl.so.7']
-	cpp_link_args = ['-L$total_install_path/lib']
+	New-Meson-Cross-File -arch "armv4" -link_flags @"
+	[
+		'-L$total_install_path/lib',
+		'-Wl,-rpath-link,$total_install_path/lib',
+	]
 "@
 
 	Set-Location $source_path
--- arm-none-linux-gnueabihf/build-wayland.ps1
+++ arm-none-linux-gnueabihf/build-wayland.ps1
@@ -27,46 +27,11 @@ try
 	New-Item -Path $build_path -ItemType Directory -Force
 	Remove-Item "$build_path/*" -Recurse -Force
 	
-
-	# '$total_install_path/lib/libexpat.so.1',
-	# '$total_install_path/lib/libxml2.so.2',
-	$c_link_args = @"
+	New-Meson-Cross-File -link_flags @"
 	[
 		'-L$total_install_path/lib',
-		'$total_install_path/lib/libffi.so.8',
-		'$total_install_path/lib/libz.so.1',
-		'$total_install_path/lib/libiconv.so.2',
-		'$total_install_path/lib/liblzma.so.5',
+		'-Wl,-rpath-link,$total_install_path/lib',
 	]
-"@.Replace("`r", " ").Replace("`n", " ").Replace("`t", " ")
-
-	Create-Text-File -Path $build_path/cross_file.ini `
-		-Content @"
-	[binaries]
-	c = 'arm-none-linux-gnueabihf-gcc'
-	cpp = 'arm-none-linux-gnueabihf-g++'
-	ar = 'arm-none-linux-gnueabihf-ar'
-	ld = 'arm-none-linux-gnueabihf-ld'
-	strip = 'arm-none-linux-gnueabihf-strip'
-	pkg-config = 'pkg-config'
-
-	[host_machine]
-	system = 'linux'
-	cpu_family = 'arm'
-	cpu = 'armv7-a'
-	endian = 'little'
-
-	[target_machine]
-	system = 'linux'
-	cpu_family = 'arm'
-	cpu = 'armv7-a'
-	endian = 'little'
-
-	[built-in options]
-	c_args = ['-I$total_install_path/include']
-	cpp_args = ['-I$total_install_path/include']
-	c_link_args = $c_link_args
-	cpp_link_args = $c_link_args
 "@
 
 	Set-Location $source_path
