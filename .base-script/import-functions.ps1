3b02a9ea750c9ac70c122e01764083c6fd4f2e29
hjc2000 <huangjincanqqmail@qq.com>
2024-03-07 15:15:07 +0800

    auto commit

--- .base-script/import-functions.ps1
+++ .base-script/import-functions.ps1
@@ -238,6 +238,15 @@ function New-Meson-Cross-File
 	)
 
 	$link_flags = $link_flags.Replace("`r", " ").Replace("`n", " ").Replace("`t", " ")
+
+	# meson 的
+	#
+	# [properties]
+	# pkg_config_libdir = '$env:PKG_CONFIG_PATH'
+	#
+	# 会在调用 pkg-config 时传入 PKG_CONFIG_LIBDIR 环境变量，并设置为 pkg_config_libdir
+	# 的值，这可以让 pkg-config 的默认查找目录，例如 /usr/lib/pkgconfig 被覆盖，从而在交叉
+	# 编译时不会使用宿主机的库。
 	Create-Text-File -Path $build_path/cross_file.ini `
 		-Content @"
 	[binaries]
--- .base-script/prepare-for-building.ps1
+++ .base-script/prepare-for-building.ps1
@@ -10,7 +10,7 @@ $repos_path = "$build_script_path/.repos"
 $libs_path = "$build_script_path/.libs"
 $total_install_path = "$build_script_path/.total-install"
 
-$env:PKG_CONFIG_PATH = "$total_install_path/lib/pkgconfig:$total_install_path/share/pkgconfig"
+$env:PKG_CONFIG_PATH = "$total_install_path/lib/pkgconfig"
 $env:LIBRARY_PATH = "$total_install_path/lib/"
 
 New-Item -Path $repos_path -ItemType Directory -Force | Out-Null
