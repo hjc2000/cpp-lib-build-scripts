21b76f18cedf39c132147b7a57bcea444673638f
hjc2000 <huangjincanqqmail@qq.com>
2024-03-07 10:20:22 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-libxml2.ps1
+++ arm-none-linux-gnueabihf/build-libxml2.ps1
@@ -41,11 +41,11 @@ try
 
 	include_directories(BEFORE "$total_install_path/include")
 	link_directories(BEFORE "$total_install_path/lib")
-	link_libraries(
-		"$total_install_path/lib/libz.so.1"
-		"$total_install_path/lib/liblzma.so.5"
-		"$total_install_path/lib/libiconv.so.2"
-	)
+	# link_libraries(
+	# 	"$total_install_path/lib/libz.so.1"
+	# 	"$total_install_path/lib/liblzma.so.5"
+	# 	"$total_install_path/lib/libiconv.so.2"
+	# )
 "@
 	
 
