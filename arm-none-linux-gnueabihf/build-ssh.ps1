5f599903201141537be8319629b9ad70416e6c39
hjc2000 <huangjincanqqmail@qq.com>
2024-03-08 16:31:27 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-ssh.ps1
+++ arm-none-linux-gnueabihf/build-ssh.ps1
@@ -2,8 +2,8 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 . $build_script_path/../.base-script/prepare-for-cross-building.ps1
 
-$source_path = "$repos_path/x264/"
-$install_path = "$libs_path/x264"
+$source_path = "$repos_path/openssh-portable/"
+$install_path = "$libs_path/ssh"
 if (Test-Path -Path $install_path)
 {
 	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
@@ -13,12 +13,15 @@ if (Test-Path -Path $install_path)
 Push-Location $repos_path
 try
 {
-	get-git-repo.ps1 -git_url "https://gitee.com/Qianshunan/x264.git"
+	get-git-repo.ps1 -git_url "https://github.com/openssh/openssh-portable.git"
+	Auto-Make
 
 	# 执行命令进行构建
 	run-bash-cmd.ps1 @"
-	set -e
 	cd $source_path
+
+	./configure -h
+	exit
 
 	./configure \
 	--prefix="$install_path" \
@@ -26,11 +29,11 @@ try
 	--disable-opencl \
 	--enable-pic \
 	--host=arm-none-linux-gnueabihf \
-	--cross-prefix=arm-none-linux-gnueabihf-
+	--cross-prefix=arm-none-linux-gnueabihf- > /dev/null
 
 	make clean
-	make -j12
-	make install
+	make -j12 > /dev/null
+	make install > /dev/null
 "@
 	if ($LASTEXITCODE)
 	{
