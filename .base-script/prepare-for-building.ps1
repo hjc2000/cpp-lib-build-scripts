f2030164c549d59d1269172587d30f0c4565c3d9
hjc2000 <huangjincanqqmail@qq.com>
2024-03-08 14:41:57 +0800

    auto commit

--- .base-script/prepare-for-building.ps1
+++ .base-script/prepare-for-building.ps1
@@ -3,9 +3,6 @@ $ErrorActionPreference = "Stop"
 # 本项目的根路径，也即 cpp-lib-build-scripts 这个目录的路径
 $project_root_path = "$build_script_path/../"
 
-. $project_root_path/.base-script/import-functions.ps1
-
-
 $repos_path = "$build_script_path/.repos"
 $libs_path = "$build_script_path/.libs"
 $total_install_path = "$build_script_path/.total-install"
@@ -22,3 +19,5 @@ $env:LIBRARY_PATH = "$total_install_path/lib/"
 New-Item -Path $repos_path -ItemType Directory -Force | Out-Null
 New-Item -Path $libs_path -ItemType Directory -Force | Out-Null
 New-Item -Path $total_install_path -ItemType Directory -Force | Out-Null
+
+. $project_root_path/.base-script/import-functions.ps1
