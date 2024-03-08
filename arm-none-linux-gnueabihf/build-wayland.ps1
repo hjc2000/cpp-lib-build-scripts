7376c6eb15e9129a67f10a986421fbb66920f76e
hjc2000 <huangjincanqqmail@qq.com>
2024-03-08 14:33:37 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-wayland.ps1
+++ arm-none-linux-gnueabihf/build-wayland.ps1
@@ -28,7 +28,7 @@ try
 		-branch_name "1.20.0"
 
 	New-Item -Path $build_path -ItemType Directory -Force
-	# Remove-Item "$build_path/*" -Recurse -Force
+	Remove-Item "$build_path/*" -Recurse -Force
 	
 
 	# 不能设置
