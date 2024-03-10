a9e072622bbca5c2c47556881f502aebadad2cc1
hjc2000 <huangjincanqqmail@qq.com>
2024-03-11 00:26:06 +0800

    auto commit

--- arm-none-linux-gnueabihf/install-dotnet.ps1
+++ arm-none-linux-gnueabihf/install-dotnet.ps1
@@ -9,7 +9,7 @@ Push-Location $repos_path
 try
 {
 	wget-repo.ps1 -workspace_dir $repos_path `
-		-repo_url "https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-aspnetcore-8.0.2-linux-arm32-binaries" `
+		-repo_url "https://download.visualstudio.microsoft.com/download/pr/272dbea2-057e-4032-9857-7e00b476ceec/3c472df94b1c3f5e0d009cbccc9256a6/aspnetcore-runtime-8.0.2-linux-arm.tar.gz" `
 		-out_dir_name "aspnetcore-8.0.2"
 
 	# # 准备好安装目录
