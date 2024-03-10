1a1b3a7a65e7a4e67a67c8c16a987b66ff2abe6f
hjc2000 <huangjincanqqmail@qq.com>
2024-03-10 14:02:33 +0800

    auto commit

---
+++ arm-none-linux-gnueabihf/build-libusb.ps1
@@ -0,0 +1,46 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+. $build_script_path/../.base-script/prepare-for-cross-building.ps1
+
+$source_path = "$repos_path/libusb/"
+$install_path = "$libs_path/libusb"
+if (Test-Path -Path $install_path)
+{
+	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
+	return 0
+}
+
+Push-Location $repos_path
+try
+{
+	get-git-repo.ps1 -git_url "https://github.com/libusb/libusb.git"
+	Set-Location $source_path
+	Auto-Make
+
+	# 执行命令进行构建
+	run-bash-cmd.ps1 @"
+	cd $source_path
+	
+	./configure -h
+	exit
+
+	./configure \
+	--prefix="$install_path" \
+	--host=arm-none-linux-gnueabihf \
+
+	make clean
+	make -j12
+	make install
+"@
+
+	if ($LASTEXITCODE)
+	{
+		throw "$source_path 编译失败"
+	}
+
+	Install-Lib -src_path $install_path -dst_path $total_install_path
+}
+finally
+{
+	Pop-Location
+}
