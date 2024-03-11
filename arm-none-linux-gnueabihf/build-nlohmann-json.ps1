102b10d10cc91417233580905409c9555804b320
hjc2000 <huangjincanqqmail@qq.com>
2024-03-11 10:05:27 +0800

    auto commit

--- arm-none-linux-gnueabihf/build-nlohmann-json.ps1
+++ arm-none-linux-gnueabihf/build-nlohmann-json.ps1
@@ -3,15 +3,16 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-cross-building.ps1
 
 $source_path = "$repos_path/json/"
-$install_path = "$libs_path/nlohmann-json/include/nlohmann"
+$install_path = "$libs_path/nlohmann-json"
 
 Push-Location $repos_path
 try
 {
 	get-git-repo.ps1 -git_url https://github.com/nlohmann/json.git
-	New-Empty-Dir -Path $install_path
+
+	New-Item -Path "$install_path/include/nlohmann" -ItemType Directory -Force
 	Copy-Item -Path "$source_path/single_include/nlohmann/json.hpp" `
-		-Destination $install_path `
+		-Destination "$install_path/include/nlohmann" `
 		-Force -Recurse
 
 	Install-Lib -src_path $install_path -dst_path $total_install_path
