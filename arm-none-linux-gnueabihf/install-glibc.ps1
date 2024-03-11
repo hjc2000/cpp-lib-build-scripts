afb07283d54680aceb2efdf190d2dd3753119a7a
hjc2000 <huangjincanqqmail@qq.com>
2024-03-11 14:43:21 +0800

    auto commit

---
+++ arm-none-linux-gnueabihf/install-glibc.ps1
@@ -0,0 +1,28 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+. $build_script_path/../.base-script/prepare-for-cross-building.ps1
+
+$source_path = "$repos_path/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-linux-gnueabihf/"
+$install_path = "$libs_path/glibc/"
+
+Push-Location $repos_path
+try
+{
+	wget-repo.ps1 -workspace_dir $repos_path `
+		-repo_url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz" `
+		-out_dir_name "./"
+
+	# # 准备好安装目录
+	# New-Item -Path "$install_path/bin" -ItemType Directory -Force
+	
+	# # 将头文件复制到安装目录
+	# Copy-Item -Path "$source_path/*" `
+	# 	-Destination "$install_path/bin" `
+	# 	-Force -Recurse
+
+	# Install-Lib -src_path $install_path -dst_path $total_install_path
+}
+finally
+{
+	Pop-Location
+}
