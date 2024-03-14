0574f2a632c78ce5c8bbec21304afdc4826e5158
hjc2000 <huangjincanqqmail@qq.com>
2024-03-14 16:26:17 +0800

    build-fftw

---
+++ msys/build-fftw.ps1
@@ -0,0 +1,55 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+
+$source_path = "$repos_path/fftw/fftw-3.3.10"
+$install_path = "$libs_path/fftw/"
+$build_path = "$source_path/build/"
+if (Test-Path -Path $install_path)
+{
+	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
+	return 0
+}
+
+Push-Location $repos_path
+try
+{
+	wget-repo.ps1 -workspace_dir $repos_path `
+		-repo_url "https://fftw.org/fftw-3.3.10.tar.gz" `
+		-out_dir_name "fftw"
+
+	New-Empty-Dir $build_path
+	Create-Text-File -Path "$build_path/toolchain.cmake" `
+		-Content @"
+	set(CMAKE_SYSTEM_NAME Windows)
+	set(CMAKE_SYSTEM_PROCESSOR x64)
+	set(CMAKE_C_COMPILER clang)
+	set(CMAKE_CXX_COMPILER clang++)
+	set(CMAKE_RC_COMPILER llvm-rc)
+"@
+
+	Set-Location $build_path
+	cmake -G "Ninja" $source_path `
+		-DCMAKE_TOOLCHAIN_FILE="$build_path/toolchain.cmake" `
+		-DCMAKE_BUILD_TYPE=Release `
+		-DCMAKE_INSTALL_PREFIX="$install_path"
+		
+	if ($LASTEXITCODE)
+	{
+		throw "$source_path 配置失败"
+	}
+	
+	ninja -j12
+	if ($LASTEXITCODE)
+	{
+		throw "$source_path 编译失败"
+	}
+
+	ninja install
+
+	Fix-Pck-Config-Pc-Path
+	Install-Lib -src_path $install_path -dst_path $total_install_path
+}
+finally
+{
+	Pop-Location
+}
