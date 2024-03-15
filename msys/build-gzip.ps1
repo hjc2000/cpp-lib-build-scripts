de64743bcaf09b5eeb0448af38dfd70180080c13
hjc2000 <huangjincanqqmail@qq.com>
2024-03-15 14:48:45 +0800

    去除--enable-year2038选项。应该不需要。

--- msys/build-gzip.ps1
+++ msys/build-gzip.ps1
@@ -1,8 +1,8 @@
 $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 
-$source_path = "$repos_path/libmodbus/"
-$install_path = "$libs_path/libmodbus"
+$source_path = "$repos_path/gzip/gzip-1.13"
+$install_path = "$libs_path/gzip"
 if (Test-Path -Path $install_path)
 {
 	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
@@ -12,7 +12,9 @@ if (Test-Path -Path $install_path)
 Push-Location $repos_path
 try
 {
-	get-git-repo.ps1 -git_url "https://github.com/stephane/libmodbus.git"
+	wget-repo.ps1 -workspace_dir $repos_path `
+		-repo_url "https://ftp.gnu.org/gnu/gzip/gzip-1.13.zip" `
+		-out_dir_name "gzip"
 
 	# 执行命令进行构建
 	run-bash-cmd.ps1 @"
