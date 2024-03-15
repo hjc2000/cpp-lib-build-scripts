ee40bfd3ade829f81cbdde7be35001959a41daf0
hjc2000 <huangjincanqqmail@qq.com>
2024-03-15 20:00:10 +0800

    build-boost

--- msys/build-boost.ps1
+++ msys/build-boost.ps1
@@ -1,6 +1,15 @@
 $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 
+$source_path = "$repos_path/boost/boost_1_84_0/boost"
+$install_path = "$libs_path/boost"
+$build_path = "$source_path/jc_build"
+if (Test-Path -Path $install_path)
+{
+	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
+	return 0
+}
+
 Push-Location $repos_path
 try
 {
@@ -8,8 +17,8 @@ try
 		-repo_url "https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_1_84_0.tar.bz2" `
 		-out_dir_name "boost"
 
-	Copy-Item -Path $repos_path/boost/boost_1_84_0/boost/ `
-		-Destination $libs_path/boost/include/boost/ `
+	Copy-Item -Path $source_path `
+		-Destination $install_path/include/boost `
 		-Force -Recurse
 
 	Install-Lib -src_path $install_path -dst_path $total_install_path
