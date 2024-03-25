f33066bb9b3d83c57f92bd94880112e168be95c3
hjc2000 <huangjincanqqmail@qq.com>
2024-03-25 18:38:39 +0800

    auto commit

--- msys/build-x265.ps1
+++ msys/build-x265.ps1
@@ -16,8 +16,18 @@ try
 	get-git-repo.ps1 -git_url "https://bitbucket.org/multicoreware/x265_git.git"
 
 	New-Empty-Dir $build_path
+	Create-Text-File -Path "$build_path/toolchain.cmake" `
+		-Content @"
+	set(CMAKE_SYSTEM_NAME Windows)
+	set(CMAKE_SYSTEM_PROCESSOR x64)
+	set(CMAKE_C_COMPILER clang)
+	set(CMAKE_CXX_COMPILER clang++)
+	set(CMAKE_RC_COMPILER llvm-rc)
+"@
+	
 	Set-Location $build_path
 	cmake -G "Ninja" $source_path `
+		-DCMAKE_TOOLCHAIN_FILE="$build_path/toolchain.cmake" `
 		-DCMAKE_INSTALL_PREFIX="${install_path}" `
 		-DCMAKE_BUILD_TYPE=Release `
 		-DENABLE_SHARED=on `
