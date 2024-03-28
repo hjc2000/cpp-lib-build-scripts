d0427e810e8bf9a52c224da52eed07e67af94096
hjc2000 <huangjincanqqmail@qq.com>
2024-03-28 14:55:52 +0800

    auto commit

--- msys/build-curl.ps1
+++ msys/build-curl.ps1
@@ -14,6 +14,10 @@ Clear-Host
 Push-Location $repos_path
 try
 {
+	# 需要执行一下，设置msys环境变量，这样 pkg-config 才能识别出这是 msys ucrt64
+	# 环境，这样才能找得到库。
+	run-bash-cmd.ps1 "true"
+	
 	Build-Dependency "build-zlib.ps1"
 	Build-Dependency "build-libiconv.ps1"
 	Build-Dependency "build-icu.ps1"
