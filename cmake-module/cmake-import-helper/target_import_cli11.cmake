b189b8ec70d1bd069f043c548e49a9277a11b35d
hjc2000 <huangjincanqqmail@qq.com>
2024-03-30 18:59:52 +0800

    target_compile_definitions(${ProjectName} PUBLIC SDL_MAIN_HANDLED)

---
+++ cmake-module/cmake-import-helper/target_import_cli11.cmake
@@ -0,0 +1,3 @@
+function(target_import_cli11 target_name)
+    add_and_install_third_party_include_dir(${target_name} ${libs_path}/cli11/include/)
+endfunction()
--- cmake-module/cmake-import-helper/target_import_sdl2.cmake
+++ cmake-module/cmake-import-helper/target_import_sdl2.cmake
@@ -2,4 +2,8 @@ function(target_import_sdl2 target_name)
     add_and_install_third_party_include_dir(${target_name} ${libs_path}/sdl2/include/SDL2)
     target_auto_link_lib(${target_name} SDL2 ${libs_path}/sdl2/lib/)
     install_dll_from_dir(${libs_path}/sdl2/bin/)
+
+    # 一旦使用了 SDL2 库，不定义这个宏的话，main 函数会被替换掉
+    # 这里需要设置为 PUBLIC 保证这个预定义能够沿着依赖链传递，否则依赖链中每个环节都要自己预定义
+    target_compile_definitions(${ProjectName} PUBLIC SDL_MAIN_HANDLED)
 endfunction()
