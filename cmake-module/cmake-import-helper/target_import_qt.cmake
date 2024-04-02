9f6a166e84eb7c528b5e6fc9dfd2547063ede9e0
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-02 16:23:03 +0800

    add_compile_options(/Zc:__cplusplus)

--- cmake-module/cmake-import-helper/target_import_qt.cmake
+++ cmake-module/cmake-import-helper/target_import_qt.cmake
@@ -1,3 +1,7 @@
+if(MSVC)
+    add_compile_options(/Zc:__cplusplus)
+endif()
+
 function(target_import_qt_core target_name)
     add_and_install_third_party_include_dir(${target_name} ${libs_path}/qt5/include)
     target_auto_link_lib(${target_name} Qt6Core ${libs_path}/qt5/lib)
