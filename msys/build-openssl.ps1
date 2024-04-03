87572e09af3fed768e55b39a640e388b9108bcac
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-03 22:44:33 +0800

    $env:Path = "C:\msys64\usr\bin\core_perl;" + $env:Path

--- msys/build-openssl.ps1
+++ msys/build-openssl.ps1
@@ -2,7 +2,7 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 . $build_script_path/../.base-script/prepare-for-msys.ps1
 
-$source_path = "$repos_path/openssl"
+$source_path = "$repos_path/openssl/openssl-3.3.0-beta1"
 $install_path = "$libs_path/openssl"
 if (Test-Path -Path $install_path)
 {
@@ -14,11 +14,9 @@ Clear-Host
 Push-Location $repos_path
 try
 {
-	get-git-repo.ps1 -git_url "https://github.com/openssl/openssl.git"
-
-	# 编译后安装阶段依赖 Perl 中的 pod2man，但是它比较不同寻常，
-	# 可执行文件不是在 bin 里面，而是在 bin 里面又创建了一个目录
-	$env:Path = "C:\msys64\usr\bin\core_perl;" + $env:Path
+	wget-repo.ps1 -workspace_dir $repos_path `
+		-repo_url "https://www.openssl.org/source/openssl-3.3.0-beta1.tar.gz" `
+		-out_dir_name "openssl"
 	
 	run-bash-cmd.ps1 @"
 	cd $(cygpath.exe $source_path)
