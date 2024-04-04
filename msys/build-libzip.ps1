6bfebbfa7a4bdaf042f2edf60d7c715abb9ea4b7
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-04 21:27:42 +0800

    build-libzip

--- msys/build-libzip.ps1
+++ msys/build-libzip.ps1
@@ -16,6 +16,8 @@ try
 {
 	Build-Dependency "build-zlib.ps1"
 	Build-Dependency "build-bzip2.ps1"
+	Build-Dependency "build-xz.ps1"
+
 	git-get-repo.ps1 -git_url "https://github.com/nih-at/libzip.git"
 
 
@@ -40,8 +42,12 @@ try
 
 	ninja install | Out-Null
 
+	Install-Msys-Dlls @(
+		"/ucrt64/bin/libzstd.dll"
+	)
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/zlib/bin"
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/bzip2/bin"
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/xz/bin"
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 	Install-Lib -src_path $install_path -dst_path $(cygpath.exe /ucrt64 -w)
 	Auto-Ldd $install_path/bin
