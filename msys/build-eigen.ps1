421a864a01156601aed7c299946dc8ba2fddbfdc
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-07 16:59:18 +0800

    auto commit

--- msys/build-eigen.ps1
+++ msys/build-eigen.ps1
@@ -2,9 +2,9 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 . $build_script_path/../.base-script/prepare-for-msys.ps1
 
-$source_path = "$repos_path/eigen/"
-$install_path = "$libs_path/eigen/"
-$build_path = "$source_path/jc_build/"
+$source_path = "$repos_path/eigen"
+$install_path = "$libs_path/eigen"
+$build_path = "$source_path/jc_build"
 if (Test-Path -Path $install_path)
 {
 	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
