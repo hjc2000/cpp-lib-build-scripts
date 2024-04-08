1fe613ce76b09b36b741cc3b1211c2f98ee8af5d
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-08 11:04:38 +0800

    target_add_source_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/include/)

--- cmake-module/cmake-import-helper/target_import_src.cmake
+++ cmake-module/cmake-import-helper/target_import_src.cmake
@@ -8,5 +8,6 @@ function(target_import_src target_name)
         # 放到项目根目录的 include 目录中的头文件会被包含，然后安装时保持目录结构安装。
         target_include_directories(${target_name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include/)
         install_dir(${CMAKE_CURRENT_SOURCE_DIR}/include include "*")
+        target_add_source_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/include/)
     endif()
 endfunction()
