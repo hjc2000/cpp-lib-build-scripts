c0d1864939b03f813dfeb23bbf2f4479a9133efb
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-08 10:53:29 +0800

    install_dir(${CMAKE_CURRENT_SOURCE_DIR}/include include "*")

--- cmake-module/cmake-import-helper/target_import_src.cmake
+++ cmake-module/cmake-import-helper/target_import_src.cmake
@@ -7,6 +7,6 @@ function(target_import_src target_name)
     if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/include/)
         # 放到项目根目录的 include 目录中的头文件会被包含，然后安装时保持目录结构安装。
         target_include_directories(${target_name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include/)
-        install_dir(${CMAKE_CURRENT_SOURCE_DIR}/include include)
+        install_dir(${CMAKE_CURRENT_SOURCE_DIR}/include include "*")
     endif()
 endfunction()
