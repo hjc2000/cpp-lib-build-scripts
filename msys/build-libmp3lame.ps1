fb91afa1423d23efc9b6c76c9a571c8ae715a6fe
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-16 15:50:12 +0800

    build-libmp3lame.ps1

---
+++ msys/build-libmp3lame.ps1
@@ -0,0 +1,44 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+. $build_script_path/../.base-script/prepare-for-msys.ps1
+
+$source_path = "$repos_path/libmp3lame/lame-3.100"
+$install_path = "$libs_path/libmp3lame"
+if (Test-Path -Path $install_path)
+{
+	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
+	return 0
+}
+
+Push-Location $repos_path
+try
+{
+	wget-repo.ps1 -workspace_dir $repos_path `
+		-repo_url "https://nchc.dl.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz" `
+		-out_dir_name "libmp3lame"
+
+	Set-Location $source_path
+	run-bash-cmd.ps1 @"
+	./configure \
+	--prefix="$(cygpath.exe $install_path)" \
+	--enable-nasm \
+	--with-gnu-ld
+
+	make clean
+	make -j12
+	make install
+"@
+
+	if ($LASTEXITCODE)
+	{
+		throw "$source_path 编译失败"
+	}
+
+	Install-Lib -src_path $install_path -dst_path $total_install_path
+	Install-Lib -src_path $install_path -dst_path $(cygpath.exe /ucrt64 -w)
+	Auto-Ldd $install_path/bin
+}
+finally
+{
+	Pop-Location
+}
