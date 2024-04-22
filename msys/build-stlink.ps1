7827a95af163b9d4eaa9439434aae0649dfcb49f
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-22 09:02:14 +0800

    stlink

---
+++ msys/build-stlink.ps1
@@ -0,0 +1,50 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+. $build_script_path/../.base-script/prepare-for-msys.ps1
+
+$source_path = "$repos_path/stlink"
+$install_path = "$libs_path/stlink"
+$build_path = "$source_path/jc_build"
+if (Test-Path -Path $install_path)
+{
+	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
+	return 0
+}
+
+Push-Location $repos_path
+try
+{
+	Build-Dependency "build-libusb.ps1"
+
+	git-get-repo.ps1 -git_url "https://github.com/stlink-org/stlink.git"
+
+	New-Empty-Dir $build_path
+	Set-Location $build_path
+	cmake -G "Ninja" $source_path `
+		-DCMAKE_C_COMPILER="gcc" `
+		-DCMAKE_CXX_COMPILER="g++" `
+		-DCMAKE_INSTALL_PREFIX="$install_path" `
+		-DCMAKE_BUILD_TYPE=Release
+		
+	if ($LASTEXITCODE)
+	{
+		throw "$source_path 配置失败"
+	}
+	
+	ninja -j12
+	if ($LASTEXITCODE)
+	{
+		throw "$source_path 编译失败"
+	}
+
+	ninja install
+
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/libusb/bin"
+	Install-Lib -src_path $install_path -dst_path $total_install_path
+	Install-Lib -src_path $install_path -dst_path $(cygpath.exe /ucrt64 -w)
+	Auto-Ldd $install_path/bin
+}
+finally
+{
+	Pop-Location
+}
