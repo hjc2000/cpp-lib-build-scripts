0b704d6016b8e4874ea0436e8e9178f97496fc26
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-30 16:46:24 +0800

    1

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_boost.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_boost.cmake
@@ -1,20 +1,3 @@
 function(target_import_boost target_name)
     target_include_directories(${target_name} PUBLIC ${libs_path}/boost/include/)
-
-    if(WIN32)
-        # 定义 windows API 版本
-        target_compile_definitions(${target_name} PRIVATE -D_WIN32_WINNT=0x0A00)
-    endif()
-endfunction()
-
-
-
-
-
-function(target_import_boost_asio target_name)
-	target_import_boost(${target_name})
-
-    if(WIN32)
-        target_link_libraries(${target_name} PUBLIC ws2_32)
-    endif()
 endfunction()
