d1918131abf99ca7e09ffed1c6ff99f30cd267aa
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-30 19:06:34 +0800

    target_compile_definitions(

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_libstm32f103_hal.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_libstm32f103_hal.cmake
@@ -1,8 +1,19 @@
 function(target_import_libstm32f103_hal target_name visibility)
+    target_compile_definitions(
+	    ${ProjectName} PUBLIC
+	    USE_HAL_DRIVER=1
+	    STM32F103xE=1
+	    STM32F10X_HD=1
+	    ARM_MATH_CM3
+	    ARM_MATH_MATRIX_CHECK
+	    ARM_MATH_ROUNDING
+	    UNALIGNED_SUPPORT_DISABLE
+    )
+
     target_include_directories(
         ${target_name}
         ${visibility}
-        ${libs_path}/target_import_libstm32f103_hal/include
+        ${libs_path}/libstm32f103-hal/include
     )
 
     target_link_libraries(
