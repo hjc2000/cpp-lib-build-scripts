5aa7f68f4141a634b16ef214c230f2808c667dbc
hjc2000 <hjc.666@foxmail.com>
2024-05-28 09:18:10 +0800

    target_import_boost

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_bsp_interface.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_bsp_interface.cmake
@@ -1,4 +1,5 @@
 include(target_import_base)
+include(target_import_boost)
 
 function(target_import_bsp_interface target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/bsp-interface/include/)
@@ -6,4 +7,5 @@ function(target_import_bsp_interface target_name visibility)
     install_dll_dir(${libs_path}/bsp-interface/bin/)
 
     target_import_base(${target_name} PUBLIC)
+    target_import_boost(${target_name} PUBLIC)
 endfunction()
