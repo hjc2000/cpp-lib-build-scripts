cb9f70a0622377b0a1bb55e556821615677897c8
hjc2000 <hjc.666@foxmail.com>
2024-05-28 17:08:12 +0800

    target_import_task

---
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_task.cmake
@@ -0,0 +1,5 @@
+function(target_import_task target_name visibility)
+    target_include_directories(${target_name} ${visibility} ${libs_path}/task/include/)
+    target_auto_link_lib(${target_name} task ${libs_path}/task/lib/)
+    install_dll_dir(${libs_path}/task/bin/)
+endfunction()
