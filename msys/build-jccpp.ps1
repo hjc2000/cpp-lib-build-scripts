de69d3e1ebb21eeda087d4e0def6f0b3b6f423ae
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-05 00:30:39 +0800

    Build-Dependency "build-base" 	Build-Dependency "build-nlohmann-json"

--- msys/build-jccpp.ps1
+++ msys/build-jccpp.ps1
@@ -14,6 +14,9 @@ if (Test-Path -Path $install_path)
 Push-Location $repos_path
 try
 {
+	Build-Dependency "build-base"
+	Build-Dependency "build-nlohmann-json"
+
 	git-get-repo.ps1 -git_url "https://github.com/hjc2000/jccpp.git"
 	
 	New-Empty-Dir $build_path
@@ -35,6 +38,8 @@ try
 
 	ninja install
 
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/base/bin"
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/build-nlohmann-json/bin"
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 }
 finally
