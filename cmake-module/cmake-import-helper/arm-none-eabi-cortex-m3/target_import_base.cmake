0ff9fa6778880881d542dc74d5e5ee3c0cdfd062
hjc2000 <hjc.666@foxmail.com>
2024-06-06 09:05:59 +0800

    target_import_base

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_base.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_base.cmake
@@ -1,4 +1,11 @@
 function(target_import_base target_name visibility)
+	if(has_thread)
+		message(STATUS "此平台拥有线程")
+		target_compile_definitions(${target_name} ${visibility} HAS_THREAD=1)
+	else()
+		message(STATUS "此平台没有线程")
+	endif()
+
     target_include_directories(${target_name} ${visibility} ${libs_path}/base/include/)
     target_auto_link_lib(${target_name} base ${libs_path}/base/lib/)
     install_dll_dir(${libs_path}/base/bin/)
