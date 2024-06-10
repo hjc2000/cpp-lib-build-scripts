6f792e57aa062e48543afdee031374ad0fd66b61
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-10 20:10:58 +0800

    include(target_import_base) include(target_import_pinvoke)

--- cmake-module/cmake-import-helper/msys/target_import_pinvoke.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_pinvoke.cmake
@@ -1,3 +1,6 @@
+include(target_import_base)
+include(target_import_pinvoke)
+
 function(target_import_pinvoke target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/pinvoke/include/)
     target_auto_link_lib(${target_name} pinvoke ${libs_path}/pinvoke/lib/)
