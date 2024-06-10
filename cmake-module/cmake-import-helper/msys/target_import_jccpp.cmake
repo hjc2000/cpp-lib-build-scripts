d07408d8fc9d4f7084309d3f8db35995f18dbcbf
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-10 20:14:17 +0800

    target_import_base(${target_name} ${visibility})

--- cmake-module/cmake-import-helper/msys/target_import_jccpp.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_jccpp.cmake
@@ -1,5 +1,9 @@
+include(target_import_base)
+
 function(target_import_jccpp target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/jccpp/include/)
     target_auto_link_lib(${target_name} jccpp ${libs_path}/jccpp/lib/)
     install_dll_dir(${libs_path}/jccpp/bin/)
+
+	target_import_base(${target_name} ${visibility})
 endfunction()
