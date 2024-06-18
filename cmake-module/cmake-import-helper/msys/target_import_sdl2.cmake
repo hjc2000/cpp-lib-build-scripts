6b2820b34f58a4e90b5907202803010d44ce6a1e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-19 00:18:11 +0800

    target_compile_definitions(${target_name} ${visibility} SDL_MAIN_HANDLED)

--- cmake-module/cmake-import-helper/msys/target_import_sdl2.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_sdl2.cmake
@@ -5,5 +5,5 @@ function(target_import_sdl2 target_name visibility)
 
     # 一旦使用了 SDL2 库，不定义这个宏的话，main 函数会被替换掉
     # 这里需要设置为 PUBLIC 保证这个预定义能够沿着依赖链传递，否则依赖链中每个环节都要自己预定义
-    target_compile_definitions(${ProjectName} ${visibility} SDL_MAIN_HANDLED)
+    target_compile_definitions(${target_name} ${visibility} SDL_MAIN_HANDLED)
 endfunction()
