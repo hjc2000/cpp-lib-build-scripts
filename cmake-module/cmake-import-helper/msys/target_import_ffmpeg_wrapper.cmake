58f1bf373643f73d64b6aa7744a6c25e1d9eeeaf
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-20 22:40:15 +0800

    ffmpeg-wrapper

--- cmake-module/cmake-import-helper/msys/target_import_ffmpeg_wrapper.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_ffmpeg_wrapper.cmake
@@ -5,7 +5,7 @@ include(target_import_pinvoke)
 
 function(target_import_ffmpeg_wrapper target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/ffmpeg-wrapper/include/)
-	target_auto_link_lib(${target_name} ffmpeg-wrapper ${libs_path}/ffmpeg/lib/)
+	target_auto_link_lib(${target_name} ffmpeg-wrapper ${libs_path}/ffmpeg-wrapper/lib/)
     install_dll_dir(${libs_path}/ffmpeg-wrapper/bin/)
 
 	target_import_src(${target_name})
