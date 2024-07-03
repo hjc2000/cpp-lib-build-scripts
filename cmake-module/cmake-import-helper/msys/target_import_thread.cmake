9f0de17a3c53dae1742b21862adb0e7db6f27e90
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-03 19:09:12 +0800

    target_import_thread

---
+++ cmake-module/cmake-import-helper/msys/target_import_thread.cmake
@@ -0,0 +1,5 @@
+function(target_import_thread target_name visibility)
+    target_include_directories(${target_name} ${visibility} ${libs_path}/thread/include/)
+    target_auto_link_lib(${target_name} thread ${libs_path}/thread/lib/)
+    install_dll_dir(${libs_path}/thread/bin/)
+endfunction()
