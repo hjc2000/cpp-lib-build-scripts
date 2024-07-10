a726843b6a25e0807eb127399a7d94a8c60c8a3d
hjc2000 <hjc.666@foxmail.com>
2024-07-10 08:48:20 +0800

    arm-none-eabi-cortex-m3\build-base.ps1

--- arm-none-eabi-cortex-m3/build-base.ps1
+++ arm-none-eabi-cortex-m3/build-base.ps1
@@ -15,6 +15,7 @@ Push-Location $repos_path
 try
 {
 	Build-Dependency "build-boost"
+	Build-Dependency "build-nlohmann-json"
 
 	git-get-repo.ps1 -git_url "https://github.com/hjc2000/base.git"
 	
@@ -37,6 +38,7 @@ try
 
 	ninja install
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/boost/bin"
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/nlohmann-json/bin"
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 }
 finally
