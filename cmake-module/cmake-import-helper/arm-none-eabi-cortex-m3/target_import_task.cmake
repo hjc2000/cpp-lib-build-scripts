e0e10566a3c627e8679cf51448ff737d941f17cb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-18 00:55:18 +0800

    # 解决循环依赖，所以前面链接一次，然后链接本库，接着后面再链接一次 	target_import_bsp_interface(${target_name} ${visibility})

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_task.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_task.cmake
@@ -1,9 +1,16 @@
 include(target_import_freertos)
+include(target_import_bsp_interface)
 
 function(target_import_task target_name visibility)
+	# 解决循环依赖，所以前面链接一次，然后链接本库，接着后面再链接一次
+	target_import_bsp_interface(${target_name} ${visibility})
+
     target_include_directories(${target_name} ${visibility} ${libs_path}/task/include/)
     target_auto_link_lib(${target_name} task ${libs_path}/task/lib/)
     install_dll_dir(${libs_path}/task/bin/)
 	
 	target_import_freertos(${target_name} ${visibility})
+
+	# 解决循环依赖，所以前面链接一次，然后链接本库，接着后面再链接一次
+	target_import_bsp_interface(${target_name} ${visibility})
 endfunction()
