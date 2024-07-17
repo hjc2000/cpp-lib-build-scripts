42cc72af8202569a2817aaa3a08605dbf52f1d21
hjc2000 <hjc.666@foxmail.com>
2024-07-17 08:44:48 +0800

    target_import_freertos(${target_name} ${visibility})

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_freertos.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_freertos.cmake
@@ -1,13 +1,4 @@
 function(target_import_freertos target_name visibility)
-    target_include_directories(
-        ${target_name}
-        ${visibility}
-        ${libs_path}/freertos/include
-    )
-
-    target_auto_link_lib(
-        ${target_name}
-        freertos
-        ${libs_path}/freertos/lib/
-    )
+    target_include_directories(${target_name} ${visibility} ${libs_path}/freertos/include)
+    target_auto_link_lib(${target_name} freertos ${libs_path}/freertos/lib/)
 endfunction()
