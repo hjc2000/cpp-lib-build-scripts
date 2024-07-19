693b64d17334417d228e3df108afbb10070fcb9f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-19 20:11:36 +0800

    删除target_link_libraries(${target_name} PUBLIC -Wl,--start-group) 这里不应该使用这个选项，而是应该交给可执行文件在最顶层使用

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_task.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_task.cmake
@@ -2,14 +2,10 @@ include(target_import_freertos)
 include(target_import_bsp_interface)
 
 function(target_import_task target_name visibility)
-	target_link_libraries(${target_name} PUBLIC -Wl,--start-group)
-
     target_include_directories(${target_name} ${visibility} ${libs_path}/task/include/)
     target_auto_link_lib(${target_name} task ${libs_path}/task/lib/)
     install_dll_dir(${libs_path}/task/bin/)
 
 	target_import_freertos(${target_name} ${visibility})
 	target_import_bsp_interface(${target_name} ${visibility})
-
-	target_link_libraries(${target_name} PUBLIC -Wl,--end-group)
 endfunction()
