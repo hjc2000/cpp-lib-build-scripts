4021202c2583922972eae8061e196b909af19fef
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-10 23:44:06 +0800

    target_import_stm32f103zet6_timer

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_stm32f103zet6_timer.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m3/target_import_stm32f103zet6_timer.cmake
@@ -3,9 +3,10 @@ include(target_import_bsp_interface)
 include(target_import_stm32f103zet6_hal)
 
 function(target_import_stm32f103zet6_timer target_name visibility)
-    target_include_directories(${target_name} ${visibility} ${libs_path}/stm32f103zet6-timer/include/)
-    target_auto_link_lib(${target_name} stm32f103zet6-timer ${libs_path}/stm32f103zet6-timer/lib/)
-    install_dll_dir(${libs_path}/stm32f103zet6-timer/bin/)
+	set(lib_name "stm32f103zet6-timer")
+    target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+    install_dll_dir(${libs_path}/${lib_name}/bin/)
 
     target_import_base(${target_name} ${visibility})
     target_import_bsp_interface(${target_name} ${visibility})
