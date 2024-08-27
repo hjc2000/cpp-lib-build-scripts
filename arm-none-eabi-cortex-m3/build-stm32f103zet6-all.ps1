8dc6d13c4e96f336c613ee686321511fc333bff5
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-27 23:45:59 +0800

    # Try-Remove-Item -Path "$libs_path/freertos"

--- arm-none-eabi-cortex-m3/build-stm32f103zet6-all.ps1
+++ arm-none-eabi-cortex-m3/build-stm32f103zet6-all.ps1
@@ -5,7 +5,7 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 try
 {
 	Try-Remove-Item -Path "$libs_path/base"
-	Try-Remove-Item -Path "$libs_path/freertos"
+	# Try-Remove-Item -Path "$libs_path/freertos"
 	Try-Remove-Item -Path "$libs_path/bsp-interface"
 	Try-Remove-Item -Path "$libs_path/c-bsp-interface"
 	Try-Remove-Item -Path "$libs_path/stm32f103zet6-gpio"
@@ -14,7 +14,7 @@ try
 	Try-Remove-Item -Path "$libs_path/task"
 
 	Build-Dependency "build-base"
-	Build-Dependency "build-freertos"
+	# Build-Dependency "build-freertos"
 	Build-Dependency "build-bsp-interface"
 	Build-Dependency "build-c-bsp-interface"
 	Build-Dependency "build-stm32f103zet6-gpio"
