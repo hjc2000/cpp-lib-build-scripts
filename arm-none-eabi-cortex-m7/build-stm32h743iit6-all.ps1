822b67ba099bdac58cc01e51dd802784cbd47bcf
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-27 23:46:35 +0800

    # Build-Dependency "build-freertos"

--- arm-none-eabi-cortex-m7/build-stm32h743iit6-all.ps1
+++ arm-none-eabi-cortex-m7/build-stm32h743iit6-all.ps1
@@ -5,6 +5,7 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 try
 {
 	Try-Remove-Item -Path "$libs_path/base"
+	# Try-Remove-Item -Path "$libs_path/freertos"
 	Try-Remove-Item -Path "$libs_path/bsp-interface"
 	Try-Remove-Item -Path "$libs_path/c-bsp-interface"
 	Try-Remove-Item -Path "$libs_path/stm32h743iit6-gpio"
@@ -13,6 +14,7 @@ try
 	Try-Remove-Item -Path "$libs_path/task"
 
 	Build-Dependency "build-base"
+	# Build-Dependency "build-freertos"
 	Build-Dependency "build-bsp-interface"
 	Build-Dependency "build-c-bsp-interface"
 	Build-Dependency "build-stm32h743iit6-gpio"
