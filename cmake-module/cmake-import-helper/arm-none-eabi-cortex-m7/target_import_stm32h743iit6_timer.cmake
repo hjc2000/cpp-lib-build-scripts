00756256ae1d099a554d680925846bc17bc4542a
hjc2000 <hjc.666@foxmail.com>
2024-08-29 15:01:08 +0800

    target_import_stm32h743iit6_timer

---
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_stm32h743iit6_timer.cmake
@@ -0,0 +1,14 @@
+include(target_import_base)
+include(target_import_bsp_interface)
+include(target_import_stm32h743iit6_hal)
+
+function(target_import_stm32h743iit6_timer target_name visibility)
+	set(lib_name "stm32h743iit6-timer")
+    target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+    install_dll_dir(${libs_path}/${lib_name}/bin/)
+
+    target_import_base(${target_name} ${visibility})
+    target_import_bsp_interface(${target_name} ${visibility})
+	target_import_stm32h743iit6_hal(${target_name} PRIVATE)
+endfunction()
