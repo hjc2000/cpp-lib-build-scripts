6cfa0a9b60f47eb0d74e22c9e3809d7f2c927ec4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-04 18:51:49 +0800

    target_import_osal

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_osal.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_osal.cmake
@@ -1,5 +1,9 @@
+include(target_import_freertos)
+
 function(target_import_osal target_name visibility)
 	set(lib_name "osal")
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
     target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+
+	target_import_freertos(${target_name} ${visibility})
 endfunction()
