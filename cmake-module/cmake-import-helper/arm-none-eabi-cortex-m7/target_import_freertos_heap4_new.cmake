42536a20eb1c0cb465fe96884608f0a35254663b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-07 00:54:37 +0800

    target_import_freertos_heap4_new

---
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_freertos_heap4_new.cmake
@@ -0,0 +1,14 @@
+include(target_import_bsp_interface)
+include(target_import_freertos)
+
+function(target_import_freertos_heap4_new target_name visibility)
+	set(lib_name "freertos-heap4-new")
+	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
+
+	target_link_libraries(${ProjectName} PUBLIC -Wl,--whole-archive)
+    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_link_libraries(${ProjectName} PUBLIC -Wl,--no-whole-archive)
+
+	target_import_bsp_interface(${target_name} ${visibility})
+	target_import_freertos(${target_name} ${visibility})
+endfunction()
