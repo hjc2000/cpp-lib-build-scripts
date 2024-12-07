76fcb51bf1dbcdc5612d791cce3acafdcbb94918
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-07 21:39:32 +0800

    stm32h743iit6-p-net

--- arm-none-eabi-cortex-m7/build-stm32h743iit6-p-net.ps1
+++ arm-none-eabi-cortex-m7/build-stm32h743iit6-p-net.ps1
@@ -15,6 +15,7 @@ Push-Location $repos_path
 try
 {
 	Build-Dependency "build-lwip"
+	Build-Dependency "build-freertos-osal"
 	git-get-repo.ps1 -git_url "https://github.com/hjc2000/stm32h743iit6-p-net.git"
 
 	New-Empty-Dir $build_path
@@ -36,6 +37,7 @@ try
 
 	ninja install
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/lwip/bin"
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/freertos-osal/bin"
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 }
 finally
