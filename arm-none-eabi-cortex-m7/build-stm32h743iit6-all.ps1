3632cfffc34dbcf8555797088281659a564f2c1b
hjc2000 <hjc.666@foxmail.com>
2024-12-18 15:40:16 +0800

    stm32h743iit6-ethternet

--- arm-none-eabi-cortex-m7/build-stm32h743iit6-all.ps1
+++ arm-none-eabi-cortex-m7/build-stm32h743iit6-all.ps1
@@ -4,6 +4,7 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 try
 {
+	Try-Remove-Item -Path "$libs_path/stm32h743iit6-ethternet"
 	Try-Remove-Item -Path "$libs_path/stm32h743iit6-isr"
 	Try-Remove-Item -Path "$libs_path/freertos-heap4-new"
 	Try-Remove-Item -Path "$libs_path/system-call"
@@ -26,6 +27,7 @@ try
 	Try-Remove-Item -Path "$libs_path/stm32h743iit6-serial"
 	Try-Remove-Item -Path "$libs_path/stm32h743iit6-clock"
 
+	Build-Dependency "build-stm32h743iit6-ethternet"
 	Build-Dependency "build-stm32h743iit6-isr"
 	Build-Dependency "build-freertos-heap4-new"
 	Build-Dependency "build-system-call"
