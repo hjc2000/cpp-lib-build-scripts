0da12a38b1de3c66cb52ce96e6354565040d8193
hjc2000 <hjc.666@foxmail.com>
2024-12-18 15:50:57 +0800

    target_import_stm32h743iit6_hal

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_stm32h743iit6_flash.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_stm32h743iit6_flash.cmake
@@ -5,13 +5,6 @@ include(target_import_task)
 include(target_import_stm32h743iit6_interrupt)
 
 function(target_import_stm32h743iit6_flash target_name visibility)
-	target_compile_definitions(
-		${target_name}
-		${visibility}
-		USE_HAL_DRIVER=1
-		STM32H743xx=1
-	)
-
 	set(lib_name stm32h743iit6-flash)
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
 	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
