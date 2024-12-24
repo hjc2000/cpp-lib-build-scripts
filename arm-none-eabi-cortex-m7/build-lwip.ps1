389873aee1dea7e2031514259082b9cfae2d95a1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-24 18:44:31 +0800

    Build-Dependency "build-bsp-interface"

--- arm-none-eabi-cortex-m7/build-lwip.ps1
+++ arm-none-eabi-cortex-m7/build-lwip.ps1
@@ -15,7 +15,7 @@ Push-Location $repos_path
 try
 {
 	Build-Dependency "build-freertos"
-	Build-Dependency "build-task"
+	Build-Dependency "build-bsp-interface"
 	git-get-repo.ps1 -git_url "https://github.com/hjc2000/lwip.git"
 
 	New-Empty-Dir $build_path
@@ -37,7 +37,7 @@ try
 
 	ninja install
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/freertos/bin"
-	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/task/bin"
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/bsp-interface/bin"
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 }
 finally
