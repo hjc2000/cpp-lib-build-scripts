67ec0ffcad8ea6e798b7c78c7db9625d7e7e9f1a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-24 18:43:39 +0800

    Build-Dependency "build-task"

--- arm-none-eabi-cortex-m7/build-lwip.ps1
+++ arm-none-eabi-cortex-m7/build-lwip.ps1
@@ -15,6 +15,7 @@ Push-Location $repos_path
 try
 {
 	Build-Dependency "build-freertos"
+	Build-Dependency "build-task"
 	git-get-repo.ps1 -git_url "https://github.com/hjc2000/lwip.git"
 
 	New-Empty-Dir $build_path
@@ -36,6 +37,7 @@ try
 
 	ninja install
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/freertos/bin"
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/task/bin"
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 }
 finally
