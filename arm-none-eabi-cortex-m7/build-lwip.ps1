daae6f41eff1509590c76e5f6b0764375aedd91e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-10 22:50:28 +0800

    Build-Dependency "build-freertos-osal"

--- arm-none-eabi-cortex-m7/build-lwip.ps1
+++ arm-none-eabi-cortex-m7/build-lwip.ps1
@@ -15,6 +15,7 @@ Push-Location $repos_path
 try
 {
 	Build-Dependency "build-freertos"
+	Build-Dependency "build-freertos-osal"
 	Build-Dependency "build-bsp-interface"
 	git-get-repo.ps1 -git_url "https://github.com/hjc2000/lwip.git"
 
@@ -37,6 +38,7 @@ try
 
 	ninja install
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/freertos/bin"
+	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/freertos-osal/bin"
 	Install-Dependent-Dlls-From-Dir -dll_dir "$libs_path/bsp-interface/bin"
 	Install-Lib -src_path $install_path -dst_path $total_install_path
 }
