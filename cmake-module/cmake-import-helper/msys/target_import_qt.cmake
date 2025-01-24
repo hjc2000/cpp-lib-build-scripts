cac8b9c2139b1c6e212b6e6cec5be78f6606ed6c
hjc2000 <hjc.666@foxmail.com>
2025-01-24 11:06:52 +0800

    target_import_qt_widgets

--- cmake-module/cmake-import-helper/msys/target_import_qt.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_qt.cmake
@@ -2,6 +2,11 @@ if(MSVC)
     add_compile_options(/Zc:__cplusplus)
 endif()
 
+
+
+
+
+
 function(target_import_qt_core target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include)
     target_auto_link_lib(${target_name} Qt6Core ${libs_path}/qt5/lib)
@@ -11,6 +16,11 @@ function(target_import_qt_core target_name visibility)
         DESTINATION bin
     )
 endfunction()
+
+
+
+
+
 
 function(target_import_qt_widgets target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include)
