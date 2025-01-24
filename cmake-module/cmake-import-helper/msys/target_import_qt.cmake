069f3f7f03d1f19dfd5487228f01dd02be16ae2d
hjc2000 <hjc.666@foxmail.com>
2025-01-24 10:51:55 +0800

    install_dir(${libs_path}/qt5/plugins/ bin "*")

--- cmake-module/cmake-import-helper/msys/target_import_qt.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_qt.cmake
@@ -5,8 +5,23 @@ endif()
 function(target_import_qt_core target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include)
     target_auto_link_lib(${target_name} Qt6Core ${libs_path}/qt5/lib)
+
     install(
         FILES ${libs_path}/qt5/bin/Qt6Core.dll
         DESTINATION bin
     )
+endfunction()
+
+function(target_import_qt_widgets target_name visibility)
+    target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include)
+
+    target_auto_link_lib(${target_name} Qt6Widgets ${libs_path}/qt5/lib)
+
+    install(
+        FILES
+		${libs_path}/qt5/bin/Qt6Widgets.dll
+        DESTINATION bin
+    )
+
+	install_dir(${libs_path}/qt5/plugins/ bin "*")
 endfunction()
