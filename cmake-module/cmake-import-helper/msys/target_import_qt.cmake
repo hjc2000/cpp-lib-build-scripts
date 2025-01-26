caf79e5778007cd73f3ed85972647f7b932996db
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-26 11:10:50 +0800

    install_dir(${libs_path}/qt5/plugins/platforms/ bin/platforms/ "*windows*")

--- cmake-module/cmake-import-helper/msys/target_import_qt.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_qt.cmake
@@ -20,5 +20,7 @@ endfunction()
 function(target_import_qt_widgets target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include)
     target_auto_link_lib(${target_name} Qt6Widgets ${libs_path}/qt5/lib)
-	install_dir(${libs_path}/qt5/plugins/platforms/ bin/platforms/ "*")
+
+	# 安装 qt 运行时插件。
+	install_dir(${libs_path}/qt5/plugins/platforms/ bin/platforms/ "*windows*")
 endfunction()
