a64f1085f233266608e2c446669611c8c3c13e29
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-31 18:20:53 +0800

    target_import_qt_opengl

--- cmake-module/cmake-import-helper/msys/target_import_qt.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_qt.cmake
@@ -20,7 +20,21 @@ endfunction()
 function(target_import_qt_widgets target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include)
     target_auto_link_lib(${target_name} Qt6Widgets ${libs_path}/qt5/lib)
+    target_auto_link_lib(${target_name} Qt6Gui ${libs_path}/qt5/lib)
+
+	target_import_qt_core(${target_name} ${visibility})
 
 	# 安装 qt 运行时插件。
 	install_dir(${libs_path}/qt5/plugins/platforms/ bin/platforms/ "*windows*")
+endfunction()
+
+
+
+
+function(target_import_qt_opengl target_name visibility)
+    target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include)
+    target_auto_link_lib(${target_name} Qt6OpenGL ${libs_path}/qt5/lib)
+    target_auto_link_lib(${target_name} Qt6OpenGLWidgets ${libs_path}/qt5/lib)
+
+	target_import_qt_widgets(${target_name} ${visibility})
 endfunction()
