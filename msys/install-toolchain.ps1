5a5fe5ba9f8a6ae074c6a39638465a2b04df1fee
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-07 00:06:18 +0800

    install-toolchain

--- msys/install-toolchain.ps1
+++ msys/install-toolchain.ps1
@@ -1,7 +1,8 @@
 Pacman-Ensure-Packages @(
 	"lua"
 	"llvm"
-	"mingw-w64-ucrt-x86_64-toolchain"
+	# "mingw-w64-ucrt-x86_64-toolchain"
 	"mingw-w64-ucrt-x86_64-nasm"
 	"mingw-w64-ucrt-x86_64-yasm"
+	"make"
 )
