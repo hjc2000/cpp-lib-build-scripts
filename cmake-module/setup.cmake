f4faa156d54c4dc87a6ee501ab9df0cbe9def136
hjc2000 <hjc.666@foxmail.com>
2025-02-07 16:12:50 +0800

    格式

--- cmake-module/setup.cmake
+++ cmake-module/setup.cmake
@@ -1,10 +1,9 @@
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 
 # 导入我的 cmake 函数
-list(
-	APPEND CMAKE_MODULE_PATH
-	$ENV{cpp_lib_build_scripts_path}/cmake-module/CMakeFunctions
-)
+list(APPEND CMAKE_MODULE_PATH
+	 $ENV{cpp_lib_build_scripts_path}/cmake-module/CMakeFunctions)
+
 include(install)
 include(collect)
 include(link)
@@ -12,45 +11,39 @@ include(source_and_headers)
 include(get_source_and_build)
 include(process)
 
-list(
-	APPEND CMAKE_MODULE_PATH
-	$ENV{cpp_lib_build_scripts_path}/cmake-module/cmake-import-helper
-	$ENV{cpp_lib_build_scripts_path}/cmake-module/cmake-import-helper/${platform}/
-	$ENV{cpp_lib_build_scripts_path}/cmake-module/platform-setup/
-)
+list(APPEND CMAKE_MODULE_PATH
+	 $ENV{cpp_lib_build_scripts_path}/cmake-module/cmake-import-helper
+	 $ENV{cpp_lib_build_scripts_path}/cmake-module/cmake-import-helper/${platform}/
+	 $ENV{cpp_lib_build_scripts_path}/cmake-module/platform-setup/)
+
 include(target_import_src)
 include("${platform}-setup")
 
 
 
-set(
-	libs_path
+set(libs_path
 	$ENV{cpp_lib_build_scripts_path}/${platform}/.libs
 	CACHE STRING "库仓库路径"
-	FORCE
-)
+	FORCE)
 
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
-	list(
-		APPEND CMAKE_INSTALL_RPATH
-		$ORIGIN/../lib
-		$ORIGIN/../usr/lib
-		$ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/lib
-		$ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/usr/lib
-	)
+	list(APPEND CMAKE_INSTALL_RPATH
+		 $ORIGIN/../lib
+		 $ORIGIN/../usr/lib
+		 $ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/lib
+		 $ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/usr/lib)
+
 	set(CMAKE_BUILD_WITH_INSTALL_RPATH true)
 
-	set(
-		CMAKE_EXE_LINKER_FLAGS
+	set(CMAKE_EXE_LINKER_FLAGS
 		"-Wl,-rpath-link,$ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/lib:$ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/usr/lib		${CMAKE_EXE_LINKER_FLAGS}"
 		CACHE STRING "Linker flags for executables"
-		FORCE
-	)
+		FORCE)
+
 	set(CMAKE_SHARED_LINKER_FLAGS
 		"-Wl,-rpath-link,$ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/lib:$ENV{cpp_lib_build_scripts_path}/${platform}/.total-install/usr/lib		${CMAKE_SHARED_LINKER_FLAGS}"
 		CACHE STRING "Linker flags for shared libraries"
-		FORCE
-	)
+		FORCE)
 endif()
 
 # 设置CMake构建时使用的线程数
