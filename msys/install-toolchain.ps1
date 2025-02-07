a0f194dd240d02ff626e3b602ca1986074cffad9
hjc2000 <hjc.666@foxmail.com>
2025-02-07 08:36:00 +0800

    prepare

--- msys/install-toolchain.ps1
+++ msys/install-toolchain.ps1
@@ -1,7 +1,11 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+. $build_script_path/prepare.ps1
+
 Pacman-Ensure-Packages @(
 	"lua"
 	"llvm"
-	# "mingw-w64-ucrt-x86_64-toolchain"
+	"mingw-w64-ucrt-x86_64-toolchain"
 	"mingw-w64-ucrt-x86_64-nasm"
 	"mingw-w64-ucrt-x86_64-yasm"
 	"make"
