28e809c88e9c22a0f2d65ed74ffb2cdf151e8248
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-09 14:29:20 +0800

    gcc

--- msys/build-poco.ps1
+++ msys/build-poco.ps1
@@ -27,8 +27,8 @@ try
 	Set-Location $build_path
 
 	cmake -G "Ninja" $source_path `
-		-DCMAKE_C_COMPILER="clang" `
-		-DCMAKE_CXX_COMPILER="clang++" `
+		-DCMAKE_C_COMPILER="gcc" `
+		-DCMAKE_CXX_COMPILER="g++" `
 		-DCMAKE_C_STANDARD=17 `
 		-DCMAKE_CXX_STANDARD=20 `
 		-DCMAKE_MC_COMPILER="C:\msys64\ucrt64\bin\windmc.exe" `
