4024a7e7b0dd5ac3556b561bf1ec3b898c3669f4
hjc2000 <hjc.666@foxmail.com>
2025-02-26 17:12:41 +0800

    target_import_qxlsx

---
+++ cmake-module/cmake-import-helper/msys/target_import_qxlsx.cmake
@@ -0,0 +1,5 @@
+function(target_import_qxlsx target_name visibility)
+	set(lib_name "QXlsx")
+	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+    target_auto_link_lib(${target_name} QXlsxQt6 ${libs_path}/${lib_name}/lib/)
+endfunction()
