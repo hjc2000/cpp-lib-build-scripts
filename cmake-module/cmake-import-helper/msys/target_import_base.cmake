85db0b6d33ef87ba8e82cb08da8cd1289b408f0e
hjc2000 <hjc.666@foxmail.com>
2025-03-05 14:50:08 +0800

    target_import_lualib

--- cmake-module/cmake-import-helper/msys/target_import_base.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_base.cmake
@@ -2,6 +2,8 @@ include(target_import_boost)
 include(target_import_nlohmann_json)
 
 function(target_import_base target_name visibility)
+	set(lib_name "base")
+
 	if(has_thread)
 		message(STATUS "此平台拥有线程")
 		target_compile_definitions(${target_name} ${visibility} HAS_THREAD=1)
@@ -9,8 +11,8 @@ function(target_import_base target_name visibility)
 		message(STATUS "此平台没有线程")
 	endif()
 
-    target_include_directories(${target_name} ${visibility} ${libs_path}/base/include/)
-    target_auto_link_lib(${target_name} base ${libs_path}/base/lib/)
+    target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
 
 	target_import_boost(${target_name} ${visibility})
 	target_import_nlohmann_json(${target_name} ${visibility})
---
+++ cmake-module/cmake-import-helper/msys/target_import_lualib.cmake
@@ -0,0 +1,5 @@
+function(target_import_lualib target_name visibility)
+	set(lib_name "lualib")
+    target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+endfunction()
