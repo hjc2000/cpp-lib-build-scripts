78a506f8b904da810810cf51b5da02b860ecd669
hjc2000 <hjc.666@foxmail.com>
2025-03-11 11:39:13 +0800

    /bin

--- cmake-module/platform-setup/arm-none-eabi-cortex-m7-setup.cmake
+++ cmake-module/platform-setup/arm-none-eabi-cortex-m7-setup.cmake
@@ -21,37 +21,37 @@ if(1)
 		FORCE)
 
 	set(CMAKE_C_COMPILER
-		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/arm-none-eabi-gcc.exe"
+		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-gcc.exe"
 		CACHE STRING "平台"
 		FORCE)
 
 	set(CMAKE_CXX_COMPILER
-		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/arm-none-eabi-g++.exe"
+		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-g++.exe"
 		CACHE STRING "平台"
 		FORCE)
 
 	set(CMAKE_ASM_COMPILER
-		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/arm-none-eabi-gcc.exe"
+		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-gcc.exe"
 		CACHE STRING "平台"
 		FORCE)
 
 	set(CMAKE_LINKER
-		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/arm-none-eabi-ld.exe"
+		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-ld.exe"
 		CACHE STRING "平台"
 		FORCE)
 
 	set(CMAKE_OBJCOPY
-		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/arm-none-eabi-objcopy.exe"
+		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-objcopy.exe"
 		CACHE STRING "平台"
 		FORCE)
 
 	set(CMAKE_SIZE
-		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/arm-none-eabi-size.exe"
+		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-size.exe"
 		CACHE STRING "平台"
 		FORCE)
 
 	set(CMAKE_STRIP
-		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/arm-none-eabi-ld.exe"
+		"$ENV{cpp_lib_build_scripts_path}/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-ld.exe"
 		CACHE STRING "平台"
 		FORCE)
 endif()
