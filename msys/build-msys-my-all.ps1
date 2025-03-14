be6627f14be9e0b05ac252f87842cd58898ec6fb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-14 21:11:16 +0800

    sdl2-wrapper

--- msys/build-msys-my-all.ps1
+++ msys/build-msys-my-all.ps1
@@ -16,6 +16,7 @@ try
 	try-remove-items --paths "$libs_path/widget"
 	try-remove-items --paths "$libs_path/can-diagnosis"
 	try-remove-items --paths "$libs_path/FatFs"
+	try-remove-items --paths "$libs_path/sdl2-wrapper"
 	$PSNativeCommandUseErrorActionPreference = $false
 
 	& "$build_script_path/build-base.ps1"
@@ -29,6 +30,7 @@ try
 	& "$build_script_path/build-widget.ps1"
 	& "$build_script_path/build-can-diagnosis.ps1"
 	& "$build_script_path/build-FatFs.ps1"
+	& "$build_script_path/build-sdl2-wrapper.ps1"
 }
 finally
 {
