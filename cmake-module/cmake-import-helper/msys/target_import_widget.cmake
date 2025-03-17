a8e1f9dd652f014e832edd3586298c37554e2d26
hjc2000 <hjc.666@foxmail.com>
2025-03-17 11:50:57 +0800

    target_add_pch(${target_name})

--- cmake-module/cmake-import-helper/msys/target_import_widget.cmake
+++ cmake-module/cmake-import-helper/msys/target_import_widget.cmake
@@ -5,6 +5,7 @@ include(target_import_qxlsx)
 function(target_import_widget target_name visibility)
 	set(lib_name "widget")
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+	target_add_pch(${target_name})
     target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
 
 	target_import_base(${target_name} ${visibility})
