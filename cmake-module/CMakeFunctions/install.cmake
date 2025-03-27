defe1f2fa79f7873eb6e17e516bdf9ae3b214adb
hjc2000 <hjc.666@foxmail.com>
2025-03-27 09:23:00 +0800

    将头文件安装为只读

--- cmake-module/CMakeFunctions/install.cmake
+++ cmake-module/CMakeFunctions/install.cmake
@@ -1,15 +1,13 @@
 # 将 ${src_dir} 目录的内容安装到 ${dst_dir} 目录下。会保持目录结构。
 # ${pattern} 是通配符，例如 "*" 匹配所有，"*.h" 匹配所有头文件。
 function(install_dir src_dir dst_dir pattern)
-    install(
-        DIRECTORY ${src_dir}/
-        DESTINATION ${dst_dir}
-        # 安装时保留原始的权限
-        USE_SOURCE_PERMISSIONS
-        FILES_MATCHING
-        # 使用提供的模式，或默认匹配所有文件
-        PATTERN ${pattern}
-    )
+    install(DIRECTORY ${src_dir}/
+        	DESTINATION ${dst_dir}
+			# 使用提供的模式，或默认匹配所有文件
+        	FILES_MATCHING
+			PATTERN ${pattern}
+			# 安装时保留原始的权限
+			USE_SOURCE_PERMISSIONS)
 endfunction()
 
 
@@ -20,8 +18,19 @@ endfunction()
 # 将一个文件夹中的头文件安装到安装目录下的 include 目录。
 # 会保持目录结构，同时会过滤，仅安装 *.h
 function(install_include_dir include_dir)
-    install_dir(${include_dir} include "*.h")
-    install_dir(${include_dir} ${libs_path}/../.total-install/include "*.h")
+	install(DIRECTORY ${include_dir}/
+			DESTINATION include
+			FILES_MATCHING
+			PATTERN "*.h"
+			PATTERN "*.hpp"
+			PERMISSIONS OWNER_READ GROUP_READ WORLD_READ)
+
+	install(DIRECTORY ${include_dir}/
+			DESTINATION ${libs_path}/../.total-install/include
+			FILES_MATCHING
+			PATTERN "*.h"
+			PATTERN "*.hpp"
+			PERMISSIONS OWNER_READ GROUP_READ WORLD_READ)
 endfunction()
 
 
@@ -30,11 +39,15 @@ endfunction()
 # 递归收集 ${header_dir} 下的所有头文件，安装到安装目录下的 include 目录。
 function(install_header_files_recurse header_dir)
     # 收集指定目录及其子目录下所有的头文件
-    file(GLOB_RECURSE header_files "${header_dir}/*.h")
+    file(GLOB_RECURSE header_files "${header_dir}/*.h" "${header_dir}/*.hpp")
 
-    # 安装所有收集到的头文件到安装前缀的include目录下
-    install(FILES ${header_files} DESTINATION include)
-    install(FILES ${header_files} DESTINATION ${libs_path}/../.total-install/include)
+    install(FILES ${header_files}
+			DESTINATION include
+			PERMISSIONS OWNER_READ GROUP_READ WORLD_READ)
+
+    install(FILES ${header_files}
+			DESTINATION ${libs_path}/../.total-install/include
+			PERMISSIONS OWNER_READ GROUP_READ WORLD_READ)
 endfunction()
 
 
