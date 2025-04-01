0c302b4acddcaa301fdde40ea87c8c64a3788c52
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-01 22:19:27 +0800

    set(lib_name "stm32h743iit6-hal")

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_stm32h743iit6_hal.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_stm32h743iit6_hal.cmake
@@ -6,7 +6,7 @@ function(target_import_stm32h743iit6_hal target_name visibility)
 		STM32H743xx=1
 	)
 
-	set(lib_name stm32h743iit6-hal)
+	set(lib_name "stm32h743iit6-hal")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
 	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
 endfunction()
