df85add2c7dd1d176b249412d914802c8df7e3c2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-01 23:48:38 +0800

    region

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/stm32h743iit6.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/stm32h743iit6.cmake
@@ -1,3 +1,4 @@
+# region target_import_stm32h743iit6_hal
 function(target_import_stm32h743iit6_hal target_name visibility)
 	target_compile_definitions(
 		${target_name}
@@ -10,10 +11,18 @@ function(target_import_stm32h743iit6_hal target_name visibility)
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
 	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
 endfunction()
+# endregion
 
 
+# region target_import_stm32h743iit6_isr
+function(target_import_stm32h743iit6_isr target_name visibility)
+	set(lib_name "stm32h743iit6-isr")
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
+endfunction()
+# endregion
 
 
+# region target_import_stm32h743iit6_peripherals
 function(target_import_stm32h743iit6_peripherals target_name visibility)
 	set(lib_name stm32h743iit6-peripherals)
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
@@ -23,30 +32,18 @@ function(target_import_stm32h743iit6_peripherals target_name visibility)
 	target_import_base(${target_name} ${visibility})
 	target_import_bsp_interface(${target_name} ${visibility})
 endfunction()
+# endregion
 
 
-
-
-
-
-
+# region target_import_stm32h743_lvgl
 function(target_import_stm32h743_lvgl target_name visibility)
 	target_include_directories(${target_name} ${visibility} ${libs_path}/stm32h743-lvgl/include/)
 	target_auto_link_lib(${target_name} stm32h743-lvgl ${libs_path}/stm32h743-lvgl/lib/)
 endfunction()
+# endregion
 
 
-
-
-function(target_import_stm32h743iit6_isr target_name visibility)
-	set(lib_name "stm32h743iit6-isr")
-	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
-endfunction()
-
-
-
-
-
+# region target_import_stm32h743iit6_p_net
 function(target_import_stm32h743iit6_p_net target_name visibility)
 	set(lib_name "stm32h743iit6-p-net")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
@@ -54,3 +51,4 @@ function(target_import_stm32h743iit6_p_net target_name visibility)
 
 	target_import_lwip(${target_name} ${visibility})
 endfunction()
+# endregion
