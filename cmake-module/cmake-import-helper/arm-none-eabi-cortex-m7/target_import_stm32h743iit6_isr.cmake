bcc529d5b36eb76d9e4d2f4ddf454dcbe85e8e23
hjc2000 <hjc.666@foxmail.com>
2025-04-01 14:26:16 +0800

    target_import_stm32h743iit6_isr

---
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/target_import_stm32h743iit6_isr.cmake
@@ -0,0 +1,15 @@
+include(target_import_base)
+include(target_import_bsp_interface)
+include(target_import_stm32h743iit6_hal)
+include(target_import_task)
+
+function(target_import_stm32h743iit6_isr target_name visibility)
+	set(lib_name "stm32h743iit6-isr")
+	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
+	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+
+	target_import_base(${target_name} PUBLIC)
+	target_import_bsp_interface(${target_name} PUBLIC)
+	target_import_task(${target_name} PUBLIC)
+	target_import_stm32h743iit6_hal(${target_name} PRIVATE)
+endfunction()
