38ad24f4d0aafa76fa7c5270764e880aec77205b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 00:43:06 +0800

    target_import_system_call

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/system_call.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/system_call.cmake
@@ -1,5 +1,5 @@
 function(target_import_system_call target_name visibility)
 	set(lib_name "system-call")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
-	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 endfunction()
