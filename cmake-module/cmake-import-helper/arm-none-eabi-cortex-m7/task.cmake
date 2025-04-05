487be635ee8399aded77fbc3ef41cb4fc2077fdd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 01:45:28 +0800

    set(lib_name "task")

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/task.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/task.cmake
@@ -1,6 +1,7 @@
 function(target_import_task target_name visibility)
-	target_include_directories(${target_name} ${visibility} ${libs_path}/task/include/)
-	target_auto_link_lib(${target_name} task ${libs_path}/task/lib/)
+	set(lib_name "task")
+	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
 
 	target_import_freertos(${target_name} ${visibility})
 	target_import_bsp_interface(${target_name} ${visibility})
