451a64dc9b2e4f47a534d1f8a7a0362bd6ad9a23
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 10:18:46 +0800

    stm32h743iit6-p-net 改为obj库

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/stm32h743iit6.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/stm32h743iit6.cmake
@@ -47,7 +47,7 @@ endfunction()
 function(target_import_stm32h743iit6_p_net target_name visibility)
 	set(lib_name "stm32h743iit6-p-net")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
-	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
 	target_import_lwip(${target_name} ${visibility})
 endfunction()
