58e168122600f03fb6d7b79b9d0edde946f797d8
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 10:13:42 +0800

    target_install_obj_dir不会将obj安装到总安装目录

--- cmake-module/CMakeFunctions/install.cmake
+++ cmake-module/CMakeFunctions/install.cmake
@@ -43,15 +43,7 @@ endfunction()
 # 安装一个项目目标编译产生的 obj 文件目录
 function(target_install_obj_dir target_name)
 	install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${target_name}.dir/"
-			DESTINATION "obj"
-			# 安装时保留原始的权限
-			USE_SOURCE_PERMISSIONS
-			FILES_MATCHING
-			PATTERN "*.o"
-			PATTERN "*.obj")
-
-	install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${target_name}.dir/"
-			DESTINATION "${total_install_path}/obj"
+			DESTINATION "${CMAKE_INSTALL_PREFIX}/obj"
 			# 安装时保留原始的权限
 			USE_SOURCE_PERMISSIONS
 			FILES_MATCHING
