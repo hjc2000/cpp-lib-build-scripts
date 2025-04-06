19787be95b517b0fdab1ac6aece8ef1ed128752f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 10:39:50 +0800

    target_import_lwip

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/lwip.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/lwip.cmake
@@ -1,7 +1,7 @@
 function(target_import_lwip target_name visibility)
 	set(lib_name "lwip")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
-	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
 	target_import_freertos_osal(${target_name} ${visibility})
 endfunction()
