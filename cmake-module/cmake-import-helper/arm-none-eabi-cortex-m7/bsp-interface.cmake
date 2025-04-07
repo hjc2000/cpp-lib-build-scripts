2e59803f3086dc0919e05810acba2f61b06fd6a7
hjc2000 <hjc.666@foxmail.com>
2025-04-07 09:21:56 +0800

    target_import_bsp_interface

--- cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/bsp-interface.cmake
+++ cmake-module/cmake-import-helper/arm-none-eabi-cortex-m7/bsp-interface.cmake
@@ -1,7 +1,7 @@
 function(target_import_bsp_interface target_name visibility)
 	set(lib_name "bsp-interface")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
-	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
 	target_import_base(${target_name} PUBLIC)
 	target_import_boost(${target_name} PUBLIC)
