6319ea8ffa4a0e4479cbb46b04cff9ee9d2e4e1f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-14 00:26:19 +0800

    target_import_src

--- cmake-module/CMakeFunctions/source_and_headers.cmake
+++ cmake-module/CMakeFunctions/source_and_headers.cmake
@@ -67,52 +67,42 @@ endfunction(target_add_pch target_name)
 # endregion
 
 
-# region target_import_src_root_path
-
-# 导入 root_path 中的源码。
-# root_path 中的源码按照标准的目录结构存放。
-function(target_import_src_root_path target_name root_path)
-	if(EXISTS ${root_path}/src/)
-		target_add_source_files_recurse(${target_name} ${root_path}/src/)
-		target_add_header_files_recurse(${target_name} PUBLIC ${root_path}/src/)
-		install_header_files_recurse(${root_path}/src/)
-	endif()
-
-	# 放到项目根目录的 include 目录中的头文件会被包含，然后安装时保持目录结构安装。
-	if(EXISTS ${root_path}/include/)
-		target_include_directories(${target_name} PUBLIC ${root_path}/include/)
-		install_include_dir(${root_path}/include)
-		target_add_source_files_recurse(${target_name} ${root_path}/include/)
-	endif()
-
-	# 导入 private_src 中的头文件和源码。
-	# private_src 中的头文件是私有的，不会被安装，同时是通过 PRIVATE 添加到包含的。
-	# 这可以用来放置内部私有的代码，实现类似 C# 中的 internal 修饰符的效果。
-	if(EXISTS ${root_path}/private_src/)
-		target_add_header_files_recurse(${target_name} PRIVATE ${root_path}/private_src/)
-		target_add_source_files_recurse(${target_name} ${root_path}/private_src/)
-		target_add_pch(${target_name})
-	endif()
-
-	if(EXISTS ${root_path}/private_include/)
-		target_include_directories(${target_name} PRIVATE ${root_path}/private_include/)
-		target_add_source_files_recurse(${target_name} ${root_path}/private_include/)
-	endif()
-
-	# 将本目标的编译产物安装到标准目录
-	target_install(${target_name})
-	target_total_install(${target_name})
-endfunction()
-
-# endregion
-
-
 # region target_import_src
 
 # 导入 ${CMAKE_CURRENT_SOURCE_DIR} 中的源码。
 # ${CMAKE_CURRENT_SOURCE_DIR} 中的源码按照标准的目录结构存放。
 function(target_import_src target_name)
-	target_import_src_root_path(${target_name} "${CMAKE_CURRENT_SOURCE_DIR}/")
+	if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/src/)
+		target_add_source_files_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/src/)
+		target_add_header_files_recurse(${target_name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src/)
+		install_header_files_recurse(${CMAKE_CURRENT_SOURCE_DIR}/src/)
+	endif()
+
+	# 放到项目根目录的 include 目录中的头文件会被包含，然后安装时保持目录结构安装。
+	if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/include/)
+		target_include_directories(${target_name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include/)
+		install_include_dir(${CMAKE_CURRENT_SOURCE_DIR}/include)
+		target_add_source_files_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/include/)
+	endif()
+
+	# 导入 private_src 中的头文件和源码。
+	# private_src 中的头文件是私有的，不会被安装，同时是通过 PRIVATE 添加到包含的。
+	# 这可以用来放置内部私有的代码，实现类似 C# 中的 internal 修饰符的效果。
+	if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/private_src/)
+		target_add_header_files_recurse(${target_name} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/private_src/)
+		target_add_source_files_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/private_src/)
+		target_add_pch(${target_name})
+	endif()
+
+	if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/private_include/)
+		target_include_directories(${target_name} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/private_include/)
+		target_add_source_files_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/private_include/)
+	endif()
+
+	# 将本目标的编译产物安装到标准目录
+	target_install(${target_name})
+	target_total_install(${target_name})
+	target_install_obj_dir(${ProjectName})
 endfunction()
 
 # endregion
