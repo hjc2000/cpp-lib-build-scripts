ffa5c7cc47f94bef1a45b146b2a2d4302b158477
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-15 19:26:04 +0800

    target_add_compile_options

--- cmake-module/setup.cmake
+++ cmake-module/setup.cmake
@@ -106,32 +106,3 @@ include("$ENV{cpp_lib_build_scripts_path}/cmake-module/platform-setup/${platform
 cmake_import_all_module("$ENV{cpp_lib_build_scripts_path}/cmake-module/cmake-import-helper/${platform}/" FALSE)
 
 # endregion
-
-
-
-
-
-# region target_set_compile_options
-
-function(target_set_compile_options target_name)
-    # 定义通用的 C/C++ 编译选项
-    set(c_cpp_flags
-        -Wall -Wextra -Wno-unused-parameter
-        -fno-strict-aliasing
-        -ffunction-sections
-        -fdata-sections
-    )
-
-    # 定义汇编器相关的编译选项
-    set(asm_flags
-        -x assembler-with-cpp
-    )
-
-    # 为目标设置通用的编译选项
-    target_compile_options(${target_name} PRIVATE ${c_cpp_flags})
-
-    # 为汇编源文件追加额外的编译选项
-    target_compile_options(${target_name} PRIVATE $<$<COMPILE_LANGUAGE:ASM>:${asm_flags}>)
-endfunction()
-
-# endregion
