887b9b63f539498c28ff3647a3e558b6f797566b
hjc2000 <hjc.666@foxmail.com>
2025-04-18 11:10:28 +0800

    add-toolchain-options

---
+++ cmake-module/platform-cmake-function/arm-none-eabi-arm9/add-toolchain-options.cmake
@@ -0,0 +1,57 @@
+function(target_add_platform_compile_and_link_options target_name)
+    set(options
+		-nostdlib)
+
+	target_compile_options(${target_name} PRIVATE ${options})
+	target_link_options(${target_name} PRIVATE ${options})
+endfunction()
+
+
+
+
+
+
+function(target_add_platform_compile_options target_name)
+    set(cpp_flags
+		-fgnu89-inline
+		-std=gnu99)
+
+    target_compile_options(${target_name} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${cpp_flags}>)
+endfunction()
+
+
+
+
+
+# region target_add_platform_link_options_when_it_is_exe
+
+function(target_add_platform_link_options_when_it_is_exe target_name)
+    get_target_property(target_type ${target_name} TYPE)
+	if(NOT "${target_type}" STREQUAL "EXECUTABLE")
+		return()
+	endif()
+
+    set(link_options
+		-Wl,-Map=out_map.map
+        -static)
+
+    target_link_options(${target_name} PRIVATE ${link_options})
+
+    # 检查链接脚本文件是否存在
+    if(EXISTS "${CMAKE_SOURCE_DIR}/link_script.ld")
+        # 文件存在，添加 -T 选项及链接脚本路径
+        target_link_options(${target_name} PRIVATE -T${CMAKE_SOURCE_DIR}/link_script.ld)
+    endif()
+endfunction()
+
+# endregion
+
+
+
+
+
+function(target_add_platform_toolchain_options target_name)
+	target_add_platform_compile_and_link_options(${target_name})
+	target_add_platform_compile_options(${target_name})
+	target_add_platform_link_options_when_it_is_exe(${target_name})
+endfunction()
