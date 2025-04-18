4493197f13cfec12f7a3d37b7b5e59929f45ed34
hjc2000 <hjc.666@foxmail.com>
2025-04-18 16:58:34 +0800

    target_add_platform_toolchain_options

--- cmake-module/platform-cmake-function/arm-none-eabi-arm9/add-toolchain-options.cmake
+++ cmake-module/platform-cmake-function/arm-none-eabi-arm9/add-toolchain-options.cmake
@@ -1,28 +1,3 @@
-function(target_add_platform_compile_and_link_options target_name)
-    set(options
-		-nostdlib)
-
-	target_compile_options(${target_name} PRIVATE ${options})
-	target_link_options(${target_name} PRIVATE ${options})
-endfunction()
-
-
-
-
-
-
-function(target_add_platform_compile_options target_name)
-    set(options
-		-fgnu89-inline
-		-std=gnu99)
-
-    target_compile_options(${target_name} PRIVATE ${options})
-endfunction()
-
-
-
-
-
 # region target_add_platform_link_options_when_it_is_exe
 
 function(target_add_platform_link_options_when_it_is_exe target_name)
@@ -51,7 +26,5 @@ endfunction()
 
 
 function(target_add_platform_toolchain_options target_name)
-	target_add_platform_compile_and_link_options(${target_name})
-	target_add_platform_compile_options(${target_name})
 	target_add_platform_link_options_when_it_is_exe(${target_name})
 endfunction()
