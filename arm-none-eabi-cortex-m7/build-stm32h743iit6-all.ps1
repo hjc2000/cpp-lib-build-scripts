8b3117bada076ebb131680b17531b783330601be
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-24 23:38:23 +0800

    删除reload-new

--- arm-none-eabi-cortex-m7/build-stm32h743iit6-all.ps1
+++ arm-none-eabi-cortex-m7/build-stm32h743iit6-all.ps1
@@ -9,7 +9,6 @@ try
 	$PSNativeCommandUseErrorActionPreference = $true
 	try-remove-items --paths "$libs_path/base"
 	try-remove-items --paths "$libs_path/bsp-interface"
-	try-remove-items --paths "$libs_path/reload-new"
 	try-remove-items --paths "$libs_path/lwip-wrapper"
 	try-remove-items --paths "$libs_path/stm32h743iit6-p-net"
 	try-remove-items --paths "$libs_path/stm32h743iit6-peripherals"
@@ -26,7 +25,6 @@ try
 
 	& "$build_script_path/build-base.ps1"
 	& "$build_script_path/build-bsp-interface.ps1"
-	& "$build_script_path/build-reload-new.ps1"
 	& "$build_script_path/build-lwip-wrapper.ps1"
 	& "$build_script_path/build-system-call.ps1"
 	& "$build_script_path/build-stm32h743iit6-hal.ps1"
