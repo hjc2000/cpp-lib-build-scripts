8aba7cf6ccff327e901d43f324cdb25208f8b9fc
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-26 19:39:21 +0800

    单纯实现接口，不提供依赖于其他库的头文件的项目的导入函数中不需要导入依赖的库

--- cmake-module/platform-cmake-function/arm-none-eabi-cortex-m7/import-helper/stm32h743iit6.cmake
+++ cmake-module/platform-cmake-function/arm-none-eabi-cortex-m7/import-helper/stm32h743iit6.cmake
@@ -19,10 +19,6 @@ function(target_import_stm32h743iit6_peripherals target_name visibility)
 	set(lib_name "stm32h743iit6-peripherals")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
-
-	target_import_stm32h743iit6_hal(${target_name} PRIVATE)
-	target_import_base(${target_name} ${visibility})
-	target_import_bsp_interface(${target_name} ${visibility})
 endfunction()
 # endregion
 
--- cmake-module/platform-cmake-function/arm-none-eabi-cortex-m7/import-helper/task.cmake
+++ cmake-module/platform-cmake-function/arm-none-eabi-cortex-m7/import-helper/task.cmake
@@ -2,6 +2,4 @@ function(target_import_task target_name visibility)
 	set(lib_name "task")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
-
-	target_import_base(${target_name} ${visibility})
 endfunction()
