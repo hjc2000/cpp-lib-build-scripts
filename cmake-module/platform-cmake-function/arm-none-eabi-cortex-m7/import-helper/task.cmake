381c21b4b070e66bf6cb384e6ae68e5349795f96
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-26 19:35:03 +0800

    target_import_task

--- cmake-module/platform-cmake-function/arm-none-eabi-cortex-m7/import-helper/task.cmake
+++ cmake-module/platform-cmake-function/arm-none-eabi-cortex-m7/import-helper/task.cmake
@@ -3,6 +3,5 @@ function(target_import_task target_name visibility)
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
-	target_import_freertos(${target_name} ${visibility})
-	target_import_bsp_interface(${target_name} ${visibility})
+	target_import_base(${target_name} ${visibility})
 endfunction()
