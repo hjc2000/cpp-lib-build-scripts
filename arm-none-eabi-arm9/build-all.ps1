796f5baa85933e7a16f79555af0ee4ee0201d375
hjc2000 <hjc.666@foxmail.com>
2025-04-28 14:56:47 +0800

    build-all.ps1

---
+++ arm-none-eabi-arm9/build-all.ps1
@@ -0,0 +1,20 @@
+$build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. $build_script_path/../.base-script/prepare-for-building.ps1
+. $build_script_path/prepare.ps1
+
+sync-sh.ps1
+
+try
+{
+	$PSNativeCommandUseErrorActionPreference = $true
+	try-remove-items --paths "$libs_path/c-bsp-interface"
+	try-remove-items --paths "$libs_path/prd"
+	$PSNativeCommandUseErrorActionPreference = $false
+
+	& "$build_script_path/build-c-bsp-interface.ps1"
+	& "$build_script_path/build-prd.ps1"
+}
+finally
+{
+	Pop-Location
+}
