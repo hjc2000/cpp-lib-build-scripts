6c6e98b57c839a102c1fa54542398a9b28da89d2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-29 19:32:04 +0800

    target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/) 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")

--- cmake-module/platform-cmake-function/arm-none-eabi-arm9/import-helper/prd.cmake
+++ cmake-module/platform-cmake-function/arm-none-eabi-arm9/import-helper/prd.cmake
@@ -1,7 +1,7 @@
 function(target_import_prd target_name visibility)
 	set(lib_name "prd")
-	target_include_directories(${target_name} ${visibility} "$ENV{cpp_lib_build_scripts_path}/arm-none-eabi-arm9-gcc14.2/.libs/${lib_name}/include/")
-	target_add_source_files_recurse(${target_name} "$ENV{cpp_lib_build_scripts_path}/arm-none-eabi-arm9-gcc14.2/.libs/${lib_name}/obj/")
+    target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
 	target_import_c_bsp_interface(${target_name} ${visibility})
 endfunction()
