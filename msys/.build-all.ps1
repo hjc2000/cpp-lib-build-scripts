d1786cee3a60ceecea9c65c85551782287d9260e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-21 15:47:54 +0800

    新增ffmpeg-wrapper等4个库的清理和构建步骤

--- msys/.build-all.ps1
+++ msys/.build-all.ps1
@@ -14,11 +14,19 @@ try
 	try-remove-items --paths "$libs_path/base"
 	try-remove-items --paths "$libs_path/widget"
 	try-remove-items --paths "$libs_path/can-diagnosis"
+	try-remove-items --paths "$libs_path/ffmpeg-wrapper"
+	try-remove-items --paths "$libs_path/sdl2-wrapper"
+	try-remove-items --paths "$libs_path/point24"
+	try-remove-items --paths "$libs_path/check-avstream"
 	$PSNativeCommandUseErrorActionPreference = $false
 
 	& "$build_script_path/build-base.ps1"
 	& "$build_script_path/build-widget.ps1"
 	& "$build_script_path/build-can-diagnosis.ps1"
+	& "$build_script_path/build-ffmpeg-wrapper.ps1"
+	& "$build_script_path/build-sdl2-wrapper.ps1"
+	& "$build_script_path/build-point24.ps1"
+	& "$build_script_path/build-check-avstream.ps1"
 }
 finally
 {
