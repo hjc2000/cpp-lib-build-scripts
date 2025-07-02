830163d617401ef0d2f27de9b93665097954f76d
hjc2000 <hjc.666@foxmail.com>
2025-07-02 09:51:58 +0800

    region

--- cmake-module/platform-modules/msys/add-toolchain-options.cmake
+++ cmake-module/platform-modules/msys/add-toolchain-options.cmake
@@ -1,8 +1,11 @@
 function(target_add_platform_toolchain_options target_name)
-    set(options
+	# region 编译器和链接器都要加的选项
+	set(options
 		-flto=auto
     )
 
 	target_compile_options(${target_name} PRIVATE ${options})
 	target_link_options(${target_name} PRIVATE ${options})
+	# endregion
+
 endfunction()
