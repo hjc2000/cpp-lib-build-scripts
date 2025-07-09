14986a02a0aa9514f82f1934a380b2b32d506c4e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-09 23:03:08 +0800

    target_import_qt_serial_port

--- cmake-module/platform-modules/msys/import-helper/qt.cmake
+++ cmake-module/platform-modules/msys/import-helper/qt.cmake
@@ -72,11 +72,13 @@ function(target_import_qwt target_name visibility)
 	target_import_qt_widgets(${target_name} ${visibility})
 endfunction()
 
+
+
 # 导入串口库
 function(target_import_qt_serial_port target_name visibility)
     target_include_directories(${target_name} ${visibility} ${libs_path}/qt5/include/QtSerialPort)
 
     target_auto_link_lib(${target_name} Qt6SerialPort ${libs_path}/qt5/lib)
 
-	target_import_qt_widgets(${target_name} ${visibility})
+	target_import_qt_core(${target_name} ${visibility})
 endfunction()
