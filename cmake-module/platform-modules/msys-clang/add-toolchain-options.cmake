af1d441ef93dd277e3e64b8deceac79f81104a13
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-25 19:48:01 +0800

    为MSYS Clang工具链添加C++编译选项以忽略特定警告

--- cmake-module/platform-modules/msys-clang/add-toolchain-options.cmake
+++ cmake-module/platform-modules/msys-clang/add-toolchain-options.cmake
@@ -37,6 +37,14 @@ function(target_add_platform_toolchain_options target_name)
     target_compile_options(${target_name} PUBLIC $<$<COMPILE_LANGUAGE:ASM>:${options}>)
 	# endregion
 
+	# region C++ 编译选项
+    set(options
+        -Wno-delete-non-abstract-non-virtual-dtor
+    )
+
+    target_compile_options(${target_name} PUBLIC $<$<COMPILE_LANGUAGE:CXX>:${options}>)
+	# endregion
+
 	# region 是可执行文件时添加的链接选项
 	get_target_property(target_type ${target_name} TYPE)
 
