8b33c4c02fae20f9aa6d3e7b05bec8889011b3dc
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-25 21:36:35 +0800

    为MSYS Clang工具链添加-Wno-deprecated-literal-operator警告抑制选项

--- cmake-module/platform-modules/msys-clang/add-toolchain-options.cmake
+++ cmake-module/platform-modules/msys-clang/add-toolchain-options.cmake
@@ -40,6 +40,7 @@ function(target_add_platform_toolchain_options target_name)
 	# region C++ 编译选项
     set(options
         # -Wno-delete-non-abstract-non-virtual-dtor
+		-Wno-deprecated-literal-operator
     )
 
     target_compile_options(${target_name} PUBLIC $<$<COMPILE_LANGUAGE:CXX>:${options}>)
