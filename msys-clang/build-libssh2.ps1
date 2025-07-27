e9badf53a477d6c041ad79f6e0d76c8d342d1c6f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-27 12:13:42 +0800

    将libssh2构建脚本中的编译器从GCC切换为Clang

--- msys-clang/build-libssh2.ps1
+++ msys-clang/build-libssh2.ps1
@@ -24,8 +24,8 @@ try
 	Set-Location $build_path
 
 	cmake -G "Ninja" $source_path `
-		-DCMAKE_C_COMPILER="gcc" `
-		-DCMAKE_CXX_COMPILER="g++" `
+		-DCMAKE_C_COMPILER="clang" `
+		-DCMAKE_CXX_COMPILER="clang++" `
 		-DCMAKE_BUILD_TYPE=Release `
 		-DCMAKE_INSTALL_PREFIX="$install_path"
 
