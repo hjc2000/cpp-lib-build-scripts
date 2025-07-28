56144209d08d4d6dca0e6ca9b0c3efcde997c2f5
hjc2000 <hjc.666@foxmail.com>
2025-07-28 17:03:38 +0800

    删除clang-cm7

--- cmake-module/platform-modules/arm-none-eabi-clang-cortex-m7/import-helper/boost.cmake
+++
@@ -1,3 +0,0 @@
-function(target_import_boost target_name visibility)
-	target_include_directories(${target_name} ${visibility}	${libs_path}/boost/include)
-endfunction()
--- cmake-module/platform-modules/arm-none-eabi-clang-cortex-m7/import-helper/nlohmann-json.cmake
+++
@@ -1,3 +0,0 @@
-function(target_import_nlohmann_json target_name visibility)
-	target_include_directories(${target_name} ${visibility} ${libs_path}/nlohmann-json/include/)
-endfunction()
