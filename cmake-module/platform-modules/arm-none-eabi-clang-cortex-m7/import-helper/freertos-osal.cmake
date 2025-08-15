dd4db6edd9eea1dd34d25052d608520975d0f807
hjc2000 <hjc.666@foxmail.com>
2025-08-15 13:42:14 +0800

    删除arm-none-eabi-clang-cortex-m7平台的CMake工具链选项和导入辅助模块

--- cmake-module/platform-modules/arm-none-eabi-clang-cortex-m7/import-helper/freertos-osal.cmake
+++
@@ -1,7 +0,0 @@
-function(target_import_freertos_osal target_name visibility)
-	set(lib_name "freertos-osal")
-	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
-	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
-
-	target_import_freertos(${target_name} ${visibility})
-endfunction()
