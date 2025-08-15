8ec7e6bfedd526962a68179f04c618f58ab14e97
hjc2000 <hjc.666@foxmail.com>
2025-08-15 13:28:48 +0800

    为STM32H723IIT6添加CMake导入辅助模块

--- cmake-module/platform-modules/arm-none-eabi-clang-cortex-m7/import-helper/stm32h723iit6.cmake
+++ cmake-module/platform-modules/arm-none-eabi-clang-cortex-m7/import-helper/stm32h723iit6.cmake
@@ -1,14 +1,14 @@
-# region target_import_stm32h743iit6_hal
+# region target_import_stm32h723iit6_hal
 
-function(target_import_stm32h743iit6_hal target_name visibility)
+function(target_import_stm32h723iit6_hal target_name visibility)
 	target_compile_definitions(
 		${target_name}
 		${visibility}
 		USE_HAL_DRIVER=1
-		STM32H743xx=1
+		STM32H723xx=1
 	)
 
-	set(lib_name "stm32h743iit6-hal")
+	set(lib_name "stm32h723iit6-hal")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 endfunction()
@@ -16,10 +16,10 @@ endfunction()
 # endregion
 
 
-# region target_import_stm32h743iit6_peripherals
+# region target_import_stm32h723iit6_peripherals
 
-function(target_import_stm32h743iit6_peripherals target_name visibility)
-	set(lib_name "stm32h743iit6-peripherals")
+function(target_import_stm32h723iit6_peripherals target_name visibility)
+	set(lib_name "stm32h723iit6-peripherals")
 	target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include)
 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 endfunction()
