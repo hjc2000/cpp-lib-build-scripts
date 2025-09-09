df475b77e2c51008a24620b9facbdfe92cbfbb53
hjc2000 <hjc.666@foxmail.com>
2025-09-09 09:42:24 +0800

    build(arm-none-eabi-arm9): 添加 ertec200p-3-flash-tool 编译脚本并集成到构建流程
    - 新增 build-ertec200p-3-flash-tool.ps1 脚本用于编译 ertec200p-3-flash-tool
    - 在 .build-all.ps1 中添加 ertec200p-3-flash-tool 的构建和安装步骤
    - 实现了 ertec200p-3-flash-tool 的自动下载、配置、编译和安装

--- arm-none-eabi-arm9/.build-all.ps1
+++ arm-none-eabi-arm9/.build-all.ps1
@@ -10,11 +10,13 @@ try
 	try-remove-items --paths "$libs_path/cb"
 	try-remove-items --paths "$libs_path/pn"
 	try-remove-items --paths "$libs_path/DevKit51"
+	try-remove-items --paths "$libs_path/ertec200p-3-flash-tool"
 	$PSNativeCommandUseErrorActionPreference = $false
 
 	& "$build_script_path/build-cb.ps1"
 	& "$build_script_path/build-pn.ps1"
 	& "$build_script_path/build-DevKit51.ps1"
+	& "$build_script_path/build-ertec200p-3-flash-tool.ps1"
 }
 finally
 {
