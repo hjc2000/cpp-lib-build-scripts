d998f31e45519557e80b6019c8b803db29a67358
hjc2000 <hjc.666@foxmail.com>
2025-09-11 10:44:55 +0800

    build: 启用函数和数据节编译选项
    - 在 arm-none-eabi-arm9 工具链选项中启用 -ffunction-sections 和 -fdata-sections
    - 这些选项可以提高代码和数据的分离度，有利于优化和调试

--- cmake-module/platform-modules/arm-none-eabi-arm9/add-toolchain-options.cmake
+++ cmake-module/platform-modules/arm-none-eabi-arm9/add-toolchain-options.cmake
@@ -2,8 +2,8 @@ function(target_add_platform_toolchain_options target_name)
 	# region 所有语言都要添加的编译选项
 	set(options
         -fno-strict-aliasing
-        # -ffunction-sections
-        # -fdata-sections
+        -ffunction-sections
+        -fdata-sections
 		-fmessage-length=0
 	)
 
