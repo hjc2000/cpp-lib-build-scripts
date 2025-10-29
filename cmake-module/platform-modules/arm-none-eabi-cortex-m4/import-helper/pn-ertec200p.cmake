0e9a5fe9bf055ea2f76556fb475457692c19ae33
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-10-30 00:39:16 +0800

    pn-ertec200p

---
+++ cmake-module/platform-modules/arm-none-eabi-cortex-m4/import-helper/pn-ertec200p.cmake
@@ -0,0 +1,7 @@
+function(target_import_pn_ertec200p target_name visibility)
+	set(lib_name "pn-ertec200p")
+    target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
+
+	target_import_cb(${target_name} ${visibility})
+endfunction()
