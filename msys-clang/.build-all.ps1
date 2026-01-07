142fb0555c3d9bca7f902514e4b17df12258e820
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-01-07 20:59:03 +0800

    ``` fix(build): 更新构建脚本中的同步命令
    将所有构建脚本中的 sync-sh.ps1 命令替换为 pull-sh.ps1，
    以确保使用正确的拉取命令进行代码同步。
    ```

--- msys-clang/.build-all.ps1
+++ msys-clang/.build-all.ps1
@@ -2,7 +2,7 @@ $build_script_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $build_script_path/../.base-script/prepare-for-building.ps1
 . $build_script_path/prepare.ps1
 
-sync-sh.ps1
+pull-sh.ps1
 
 try
 {
