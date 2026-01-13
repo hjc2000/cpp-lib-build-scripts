4238849ad7c50122d52fb2c7edf236ea2e458a97
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-01-13 22:52:29 +0800

    ``` feat(cpp-project): 修改CMake配置中的可执行文件目标名称
    将测试程序的目标名称从固定的"test"改为动态的"${ProjectName}-exe"，
    使项目构建更加灵活和规范。
    ```

--- 工具链配置文件/C++项目根目录配置文件/project.cmake
+++ 工具链配置文件/C++项目根目录配置文件/project.cmake
@@ -7,10 +7,10 @@ target_import_src(${ProjectName})
 # 添加测试程序
 if(("${platform}" STREQUAL "msys") OR
    ("${platform}" STREQUAL "msys-clang"))
-	set(test_exe_target_name "test")
-	add_executable(${test_exe_target_name})
-	target_import_test(${test_exe_target_name})
+	set(exe_target_name "${ProjectName}-exe")
+	add_executable(${exe_target_name})
+	target_import_test(${exe_target_name})
 
-	target_import_base(${test_exe_target_name} PUBLIC)
-	target_link_libraries(${test_exe_target_name} PUBLIC "${ProjectName}")
+	target_import_base(${exe_target_name} PUBLIC)
+	target_link_libraries(${exe_target_name} PUBLIC "${ProjectName}")
 endif()
