3a67551d84b70ab09641c45e0f0154738f64d6d9
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-01-18 02:12:59 +0800

    ``` feat(build): 添加base-filesystem模块依赖
    为widget组件添加了base-filesystem模块的导入支持，在MSYS和MSYS-Clang平台的
    构建脚本中加入了filesystem模块的构建步骤，并在widget的CMake配置文件中
    增加了filesystem库的链接配置。
    ```

--- cmake-module/platform-modules/msys-clang/import-helper/widget.cmake
+++ cmake-module/platform-modules/msys-clang/import-helper/widget.cmake
@@ -4,6 +4,7 @@ function(target_import_widget target_name visibility)
     target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
 
 	target_import_base(${target_name} ${visibility})
+	target_import_base_filesystem(${ProjectName} PUBLIC)
 	target_import_qt_widgets(${target_name} PUBLIC)
 	target_import_qt_opengl(${target_name} PUBLIC)
 	target_import_qwt(${target_name} PUBLIC)
--- msys-clang/build-widget.ps1
+++ msys-clang/build-widget.ps1
@@ -15,6 +15,7 @@ Push-Location $repos_path
 try
 {
 	& "$build_script_path/build-base.ps1"
+	& "$build_script_path/build-base-filesystem.ps1"
 	& "$build_script_path/build-qt5.ps1"
 	& "$build_script_path/build-zlib.ps1"
 	& "$build_script_path/build-libpng.ps1"
