7c39468407d704d6d5f5b95bcd045aebfbf4d0b3
hjc2000 <hjc.666@foxmail.com>
2026-01-22 11:18:16 +0800

    ``` feat(msys-clang): 改进构建脚本的目录管理和错误处理
    重构 build-base.ps1 脚本以改进工作目录管理，
    使用 Push-Location 和 try-catch 块来更好地处理
    目录切换和异常情况，提高脚本的稳定性和可靠性。
    ```

--- msys-clang/build-base.ps1
+++ msys-clang/build-base.ps1
@@ -1,21 +1,23 @@
-$build_script_path = get-script-dir.ps1
-. $build_script_path/../.base-script/prepare-for-building.ps1
-. $build_script_path/prepare.ps1
-
-$source_path = "$repos_path/base"
-$install_path = "$libs_path/base"
-$build_path = "$source_path/jc_build"
-
-if (Test-Path -Path $install_path)
-{
-	Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
-	return 0
-}
-
-Push-Location $repos_path
+Push-Location
 
 try
 {
+	$build_script_path = get-script-dir.ps1
+	. $build_script_path/../.base-script/prepare-for-building.ps1
+	. $build_script_path/prepare.ps1
+
+	$source_path = "$repos_path/base"
+	$install_path = "$libs_path/base"
+	$build_path = "$source_path/jc_build"
+
+	if (Test-Path -Path $install_path)
+	{
+		Write-Host "$install_path 已存在，不编译，直接返回。如需编译，请先删除目录。"
+		return 0
+	}
+
+	Set-Location $repos_path
+
 	& "$build_script_path/build-boost.ps1"
 	& "$build_script_path/build-nlohmann-json.ps1"
 
