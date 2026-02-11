812de53bcb41211c272c5f6864d6138fe956df22
hjc2000 <hjc.666@foxmail.com>
2026-02-11 10:10:49 +0800

    ``` feat(cmake-module): 修改MSYS-Clang平台模块的基础文件系统导入方式
    将基础文件系统的链接方式从自动链接库改为递归添加源文件，
    以更好地支持MSYS-Clang环境下的构建需求。
    ```

--- cmake-module/platform-modules/msys-clang/import-helper/base-filesystem.cmake
+++ cmake-module/platform-modules/msys-clang/import-helper/base-filesystem.cmake
@@ -2,5 +2,5 @@ function(target_import_base_filesystem target_name visibility)
 	set(lib_name "msys-base-filesystem")
 
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
-    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 endfunction()
