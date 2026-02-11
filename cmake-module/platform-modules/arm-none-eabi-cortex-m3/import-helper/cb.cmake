24595dcd5e240aa2a7f66e2c2c7325649b2302de
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-11 14:19:51 +0800

    ``` build(cmake): 统一各平台cb库导入方式
    在多个平台的cmake模块中添加空行以保持代码格式一致性，
    并将msys和msys-clang平台的库链接方式从target_auto_link_lib
    改为target_add_source_files_recurse，与其他arm平台保持一致。
    ```

--- cmake-module/platform-modules/arm-none-eabi-cortex-m3/import-helper/cb.cmake
+++ cmake-module/platform-modules/arm-none-eabi-cortex-m3/import-helper/cb.cmake
@@ -1,5 +1,6 @@
 function(target_import_cb target_name visibility)
 	set(lib_name "cb")
+
     target_include_directories(${target_name} ${visibility} "${libs_path}/${lib_name}/include/")
 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 endfunction()
--- cmake-module/platform-modules/msys-clang/import-helper/cb.cmake
+++ cmake-module/platform-modules/msys-clang/import-helper/cb.cmake
@@ -1,5 +1,6 @@
 function(target_import_cb target_name visibility)
 	set(lib_name "cb")
+
     target_include_directories(${target_name} ${visibility} "${libs_path}/${lib_name}/include/")
-    target_auto_link_lib(${target_name} ${lib_name} "${libs_path}/${lib_name}/lib/")
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 endfunction()
