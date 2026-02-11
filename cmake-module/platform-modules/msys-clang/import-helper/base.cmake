e6ea6a2746089a3f20313dcf84ed8979bea11daf
hjc2000 <hjc.666@foxmail.com>
2026-02-11 11:39:54 +0800

    ``` feat(cmake): 修改MSYS平台模块的基础导入配置
    将目标链接方式从自动链接库文件改为添加源文件递归，
    以便更好地支持对象文件的导入处理
    ```

--- cmake-module/platform-modules/msys-clang/import-helper/base.cmake
+++ cmake-module/platform-modules/msys-clang/import-helper/base.cmake
@@ -9,7 +9,7 @@ function(target_import_base target_name visibility)
 	endif()
 
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
-    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
 	target_import_boost(${target_name} ${visibility})
 	target_import_nlohmann_json(${target_name} ${visibility})
