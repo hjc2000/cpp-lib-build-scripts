2f06094ec9a75751dd80904500dd005c95b54f31
hjc2000 <hjc.666@foxmail.com>
2026-02-11 11:40:06 +0800

    ``` feat(cmake-module): 添加base-filesystem导入基础模块支持
    在MSYS2 Clang平台的base-filesystem模块中添加了对base模块的导入功能，
    通过调用target_import_base函数实现基础依赖的正确链接和配置。
    ```

--- cmake-module/platform-modules/msys-clang/import-helper/base-filesystem.cmake
+++ cmake-module/platform-modules/msys-clang/import-helper/base-filesystem.cmake
@@ -3,4 +3,6 @@ function(target_import_base_filesystem target_name visibility)
 
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
 	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
+
+	target_import_base(${target_name} PUBLIC)
 endfunction()
