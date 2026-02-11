e14efb56dfd026a0fc0acde60e4ddc2457d7a05d
hjc2000 <hjc.666@foxmail.com>
2026-02-11 11:50:52 +0800

    ``` refactor(cmake-module): 重构源文件递归添加函数
    移除了针对可执行文件类型的特殊处理逻辑，统一了源文件收集方式。
    现在无论目标类型如何，都会收集所有 C、C++ 和汇编源文件，
    并将目标文件(.o、.obj)的收集与可执行文件类型解耦。
    ```

--- cmake-module/modules/source_and_headers.cmake
+++ cmake-module/modules/source_and_headers.cmake
@@ -29,21 +29,18 @@ endfunction()
 function(target_add_source_files_recurse target_name source_path)
 	get_target_property(target_type ${target_name} TYPE)
 
+	file(GLOB_RECURSE source_file_list
+		"${source_path}/*.c"
+		"${source_path}/*.cpp"
+		"${source_path}/*.s")
+
+	target_sources(${target_name} PRIVATE ${source_file_list})
+
 	# 判断项目类型是否为可执行文件
 	if("${target_type}" STREQUAL "EXECUTABLE")
-		# 如果是可执行文件，收集所有指定的源文件，包括 .obj 文件
 		file(GLOB_RECURSE source_file_list
-			"${source_path}/*.c"
-			"${source_path}/*.cpp"
-			"${source_path}/*.s"
 			"${source_path}/*.o"
 			"${source_path}/*.obj")
-	else()
-		# 如果不是可执行文件，不收集 .obj 文件
-		file(GLOB_RECURSE source_file_list
-			"${source_path}/*.c"
-			"${source_path}/*.cpp"
-			"${source_path}/*.s")
 	endif()
 
 	target_sources(${target_name} PRIVATE ${source_file_list})
