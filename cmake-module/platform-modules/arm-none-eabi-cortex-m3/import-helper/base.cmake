e28224ed08d699d63ff922f837030363641c60cc
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-11 14:15:57 +0800

    ``` fix(cmake-module): 修复CMake模块中路径参数的引号问题
    在多个平台模块的import-helper文件中，为target_auto_link_lib函数调用中的库路径参数添加了
    必要的双引号包围，以确保路径字符串的正确传递和解析。
    ```

--- cmake-module/platform-modules/arm-none-eabi-cortex-m3/import-helper/base.cmake
+++ cmake-module/platform-modules/arm-none-eabi-cortex-m3/import-helper/base.cmake
@@ -9,7 +9,7 @@ function(target_import_base target_name visibility)
 	endif()
 
     target_include_directories(${target_name} ${visibility} "${libs_path}/${lib_name}/include/")
-    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+    target_auto_link_lib(${target_name} ${lib_name} "${libs_path}/${lib_name}/lib/")
 
 	target_import_boost(${target_name} ${visibility})
 	target_import_nlohmann_json(${target_name} ${visibility})
