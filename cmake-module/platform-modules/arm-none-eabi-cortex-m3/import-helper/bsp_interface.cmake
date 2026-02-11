1f47fd1b430ae3b4a175f91add9d2e6427bfa22e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-11 14:12:24 +0800

    ``` fix(cmake): 统一BSP接口导入函数中的依赖库可见性参数
    将arm-none-eabi-cortex-m3、m4和m7平台的bsp_interface.cmake文件中，
    target_import_base和target_import_boost函数调用的可见性参数从硬编码的PUBLIC
    改为使用函数传入的visibility参数，确保依赖库的可见性设置与目标一致。
    ```

--- cmake-module/platform-modules/arm-none-eabi-cortex-m3/import-helper/bsp_interface.cmake
+++ cmake-module/platform-modules/arm-none-eabi-cortex-m3/import-helper/bsp_interface.cmake
@@ -2,6 +2,6 @@ function(target_import_bsp_interface target_name visibility)
 	target_include_directories(${target_name} ${visibility} ${libs_path}/bsp-interface/include/)
 	target_auto_link_lib(${target_name} bsp-interface ${libs_path}/bsp-interface/lib/)
 
-	target_import_base(${target_name} PUBLIC)
-	target_import_boost(${target_name} PUBLIC)
+	target_import_base(${target_name} ${visibility})
+	target_import_boost(${target_name} ${visibility})
 endfunction()
