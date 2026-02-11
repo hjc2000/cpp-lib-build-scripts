35e60a3a37adb70b7e283583b459d0fa50e395fd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-11 13:58:14 +0800

    ``` feat(cmake): 修改SDL2包装器的链接方式
    将msys和msys-clang平台模块中的SDL2包装器从自动链接库文件
    改为递归添加源文件，以支持对象文件的直接编译集成。
    ```

--- cmake-module/platform-modules/msys/import-helper/sdl2-wrapper.cmake
+++ cmake-module/platform-modules/msys/import-helper/sdl2-wrapper.cmake
@@ -1,7 +1,8 @@
 function(target_import_sdl2_wrapper target_name visibility)
 	set(lib_name "sdl2-wrapper")
+
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
-	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
 	target_import_sdl2(${target_name} ${visibility})
 	target_import_base(${target_name} ${visibility})
