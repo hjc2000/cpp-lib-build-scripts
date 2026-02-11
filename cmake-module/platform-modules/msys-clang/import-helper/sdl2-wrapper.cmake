48a0a3c258fe5b08196662d935f735848cb46448
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-11 13:57:51 +0800

    空行

--- cmake-module/platform-modules/msys-clang/import-helper/sdl2-wrapper.cmake
+++ cmake-module/platform-modules/msys-clang/import-helper/sdl2-wrapper.cmake
@@ -1,5 +1,6 @@
 function(target_import_sdl2_wrapper target_name visibility)
 	set(lib_name "sdl2-wrapper")
+
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
 	target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
 
