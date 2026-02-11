5dff15fa22418142207722f09de82d027eb5e81a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-11 14:02:07 +0800

    ``` build(widget): 修改widget库的构建配置
    在MSYS和MSYS-Clang平台模块中，将widget库的链接方式从自动链接库文件
    改为递归添加源文件。通过target_add_source_files_recurse函数直接
    引入libs/widget/obj/目录下的源文件，而不是使用target_auto_link_lib
    链接lib目录下的库文件。同时在函数中添加了空行以改善代码可读性。
    ```

--- cmake-module/platform-modules/msys/import-helper/widget.cmake
+++ cmake-module/platform-modules/msys/import-helper/widget.cmake
@@ -1,7 +1,8 @@
 function(target_import_widget target_name visibility)
 	set(lib_name "widget")
+
     target_include_directories(${target_name} ${visibility} ${libs_path}/${lib_name}/include/)
-    target_auto_link_lib(${target_name} ${lib_name} ${libs_path}/${lib_name}/lib/)
+	target_add_source_files_recurse(${target_name} "${libs_path}/${lib_name}/obj/")
 
 	target_import_base(${target_name} ${visibility})
 	target_import_base_filesystem(${ProjectName} PUBLIC)
